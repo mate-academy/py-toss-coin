357e4db8758b0d9931a169f76f54f1ff94ee4278
polinatytarenko <p.tytarenko.20@gmail.com>
2022-02-08 00:43:33 +0200

    Add flip_coin function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import random
+from collections import Counter
+
+
+def flip_coin():
+    head_appearing_frequency = Counter(
+        [random.choice(["head", "tail"]) for _ in range(10)].count("head")
+        for _ in range(10000)
+    )
+
+    head_appearing_percentage = {
+        key: round(value / 100)
+        for key, value in sorted(head_appearing_frequency.items())
+    }
+
+    return head_appearing_percentage
