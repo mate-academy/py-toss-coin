a5903946aba76c3991276ca1493ffd95750a9036
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-02-07 15:22:01 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+import random
+
+
+def flip_coin():
+    chance_dict = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0,
+                   5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
+    for _ in range(10000):
+        num = random.randint(0, 10)
+        chance_dict[num] += 1
+
+    return chance_dict
