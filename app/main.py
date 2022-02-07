5c4f0266d50a4aac322bc49864c950868e1d054a
Alex Gergel <alexgergel@gmail.com>
2022-02-08 00:28:59 +0200

    "Now it works as intended (I hope)"

--- app/main.py
+++ app/main.py
@@ -9,6 +9,8 @@ def create_dictionary():
     for iteration in range(10000):
         flips = [random.randrange(0, 2, 1) for i in range(10)]
         dict_of_occurrences[flips.count(1)] += 1
+    for key, value in dict_of_occurrences.items():
+        dict_of_occurrences[key] = value // 100
     return dict_of_occurrences
 
 
@@ -21,7 +23,7 @@ def draw_gauss_curve():
 
         ax.scatter(create_dictionary().keys(), create_dictionary().values())
         plt.xlabel('Key', fontsize=10, color="green")
-        plt.ylabel('Count of occurrences', fontsize=10, color="blue")
+        plt.ylabel('% of occurrences', fontsize=10, color="blue")
 
         mu = np.mean(list_of_occurrences)
         sigma = np.std(list_of_occurrences)
@@ -29,7 +31,7 @@ def draw_gauss_curve():
         u = np.linspace(mu - 4 * sigma, mu + 4 * sigma, 100)
         ax2 = ax.twinx()
         ax2.plot(u, stats.norm.pdf(u, mu, sigma), color='crimson')
-        ax2.set_ylabel('normal curve')
+        ax2.set_ylabel('Normal curve')
 
         plt.show()
     except KeyboardInterrupt:
