88de44ce8c53f85b5f4f497792a739a24b942f22
alix makarova <alix.makarova@gmail.com>
2022-02-08 12:45:30 +0200

    used comprehensions

--- app/main.py
+++ app/main.py
@@ -2,9 +2,8 @@ import random
 
 
 def flip_coin():
-    dict_flip_result = {
-        0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0
-    }
+    dict_flip_result = {i: 0 for i in range(11)}
+
     for i in range(10000):
         flips = [random.randint(0, 1) for x in range(10)]
         count = sum(flips)
