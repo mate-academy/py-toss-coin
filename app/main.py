e739f778ac39257f595f2c4c0001cbc1e50dfc0f
Anita Posledova <anitaposliedova@gmail.com>
2022-02-17 13:44:18 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import random
+
+
+def flip_coin():
+    cases_of_flipping = 10000
+    number_of_flips = 10
+    results = {i: 0 for i in range(number_of_flips + 1)}
+
+    for _ in range(cases_of_flipping):
+        flips = {1: 0, 0: 0}
+        for flip_index in range(number_of_flips):
+            flips[random.randint(0, 1)] += 1
+        results[flips[1]] += 1
+
+    for key in results:
+        results[key] = int((results[key] / cases_of_flipping) * 100)
+
+    return results
