c9224881645cce84a90a1f44e96ddf4193681d46
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-05-30 14:58:57 +0300

    Solution first part of task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+import random
+
+
+def flip_coin():
+    result = {i: 0 for i in range(11)}
+    for _ in range(10000):
+        result[sum([random.randint(0, 1) for n in range(10)])] += 1
+    return {key: int(value / 100) for key, value in result.items()}
+
+
+print(flip_coin())
