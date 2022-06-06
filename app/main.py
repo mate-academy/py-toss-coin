f2ee45fde428be9eee13258cd6dbf5128e0dae00
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-06-06 23:02:59 +0300

    Fix remarks once more

--- app/main.py
+++ app/main.py
@@ -1,20 +1,29 @@
 import random
 import matplotlib.pyplot as pl
+from matplotlib.ticker import MultipleLocator
 
 
 def flip_coin():
     result = {i: 0 for i in range(11)}
     for _ in range(10000):
         result[sum([random.randint(0, 1) for n in range(10)])] += 1
-    return {key: int(value / 100) for key, value in result.items()}
+    return {key: int((value * 100) / 10000) for key, value in result.items()}
 
 
 def draw_gaussian_distribution_graph():
-    odds_dict = flip_coin()
-    pl.plot(odds_dict.keys(), odds_dict.values())
-    pl.xlim(0, 10)
-    pl.ylim(0, 100)
+    data = flip_coin()
+    x = list(data.keys())
+    y = list(data.values())
+    fig, ax = pl.subplots()
+    pl.plot(x, y, color="b")
     pl.xlabel("Heads count", fontsize=10)
     pl.ylabel("Drop percentage %", fontsize=10)
     pl.title("Gaussian distribution")
+    ax.xaxis.set_major_locator(MultipleLocator(1))
+    ax.xaxis.set_minor_locator(MultipleLocator(1))
+    ax.yaxis.set_major_locator(MultipleLocator(10))
+    ax.yaxis.set_minor_locator(MultipleLocator(5))
+    pl.axis([0, 10, 0, 100])
+    pl.xticks(range(11))
+    pl.yticks(range(0, 101, 10))
     pl.show()
