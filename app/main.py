07ad592e4a10f439ba8d72f667b6d47d98b97ea9
Dasha Protsko <protscodasha@icloud.com>
2022-06-07 15:36:17 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import random
+
+
+def flip_coin():
+    res = {el: 0 for el in range(11)}
+
+    for _ in range(1000):
+        flips = 0
+
+        for _ in range(10):
+            face_side = random.randint(0, 1)
+            if face_side:
+                flips += 1
+
+        res[flips] += 1
+
+    return {key: value / 100 for key, value in res.tems()}
