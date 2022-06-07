b57e8fb5f18ccf19a4edc92236029a40dc12cf38
Yurnerroo <yura.zdanevych@gmail.com>
2022-06-07 14:02:35 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import random
+
+
+def flip_coin():
+    result = {i: 0 for i in range(11)}
+
+    for i in range(10000):
+        flip = 0
+
+        for j in range(10):
+            face = random.randint(0, 1)
+            if face:
+                flip += 1
+
+        result[flip] += 1
+
+    return {key: value / 100 for key, value in result.items()}
