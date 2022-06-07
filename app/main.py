8fe38e16c2fe892df2bdfdd9e8c42491ddbd53de
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-06-07 11:40:21 +0300

    Remove int

--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ from matplotlib.ticker import MultipleLocator
 def flip_coin():
     result = {i: 0 for i in range(11)}
     for _ in range(10000):
-        result[sum([random.randint(0, 1) for n in range(10)])] += 1
-    return {key: int((value * 100) / 10000) for key, value in result.items()}
+        result[sum([random.randint(0, 1) for _ in range(10)])] += 1
+    return {key: (value / 100) for key, value in result.items()}
 
 
 def draw_gaussian_distribution_graph():
