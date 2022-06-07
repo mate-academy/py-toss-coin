be261a4550407f2f88148c463f5ea49b14360854
andrii kravchenko <andriej.krawczenko@gmail.com>
2022-06-07 11:41:40 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,12 +2,14 @@ import random
 
 
 def flip_coin():
-    result = {i: 0 for i in range(0, 11)}
+    result = {number: 0 for number in range(11)}
     for _ in range(10000):
-        flips = ['Heads' if x == 1 else 'Tails' for x in
+        flips = ['Heads' if flip == 1 else 'Tails' for flip in
                  [random.randint(0, 1) for _ in range(10)]]
         heads = flips.count('Heads')
         result[heads] += 1
-    for k, v in result.items():
-        result[k] = round(v / 100, 2)
+    for number, times in result.items():
+        result[number] = round(times / 100, 2)
     return result
+
+print(flip_coin())
