8fa4f5cea95c69db003d78b8123f42567a4b392a
Dima Svirsa <august4smile@gmail.com>
2022-06-07 11:35:54 +0300

    persents

--- app/main.py
+++ app/main.py
@@ -5,4 +5,4 @@ def flip_coin():
     heads_count = {i: 0 for i in range(11)}
     for _ in range(10000):
         heads_count[sum(random.randint(0, 1) for _ in range(10))] += 1
-    return heads_count
+    return {key: value / 100 for key, value in heads_count.items()}
