881c152d218da50c4c4a6974fc2e9c313a1fc34c
Олег Белый <belochard@gmail.com>
2022-06-07 19:36:01 +0300

    Model with graph

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import random
+from matplotlib import pyplot as plt
 
 
 def flip_coin():
@@ -9,8 +10,22 @@ def flip_coin():
     for _ in range(10000):
         heads = 0
         for i in range(10):
-            if random.randint(0, 10) & 1:
+            if random.randint(0, 1):
                 heads += 1
         outcomes[heads] += 1
 
     return {key: round(value * 0.01, 2) for (key, value) in outcomes.items()}
+
+
+def draw_gaussian_distribution_graph():
+    percents = [percent for percent in flip_coin().values()]
+    plt.title("Gaussian distribution")
+    plt.xlabel("Drop out of 10 attempts")
+    plt.ylabel("Chance of occurring")
+    plt.plot(percents, color="blue", linewidth=2)
+    plt.show()
+
+
+draw_gaussian_distribution_graph()
+
+
