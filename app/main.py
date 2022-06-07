48d9214761583a3ebf1cac4ec9ff279473fc3c99
Katya Vasylenko <kettyblack20@gmail.com>
2022-06-07 15:39:59 +0300

    flake8 fixed Solution

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ import random
 
 
 def flip_coin():
-    dict_of_causes = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
+    dict_of_causes = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0,
+                      5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
     for _ in range(10000):
         count_of_head = 0
 
