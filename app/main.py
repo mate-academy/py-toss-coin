7ace89f62ac5a8c83c156acc59351947c85fb137
Daria <darizhur.vol2@gmail.com>
2022-06-07 15:27:17 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import random
+
+
+def flip_coin():
+    result = {i: 0 for i in range(11)}
+    for _ in range(10000):
+        flips = 0
+        for _ in range(10):
+            toss = random.randint(0, 1)
+            if toss == 1:
+                flips += 1
+        result[flips] += 1
+
+    return {key: value / 100 for key, value in result.items()}
+
+
+print(flip_coin())
