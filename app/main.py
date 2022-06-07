6a5e91c2e707242c3471f45e5c9985000c5c4a4e
Katya Vasylenko <kettyblack20@gmail.com>
2022-06-07 22:01:34 +0300

    Fixed Solution

--- app/main.py
+++ app/main.py
@@ -2,12 +2,11 @@ import random
 
 
 def flip_coin():
-    dict_of_causes = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0,
-                      5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
+    dict_of_causes = {num: 0 for num in range(11)}
     for _ in range(10000):
         count_of_head = 0
 
-        for i in range(10):
+        for _ in range(10):
             if random.randint(1, 2) == 1:
                 count_of_head += 1
 
