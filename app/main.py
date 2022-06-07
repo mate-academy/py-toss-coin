ab64d90ebc9829ff6da95892086975124ada7db7
Yura Beseda <yuriy.beseda@gmail.com>
2022-06-07 16:40:35 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import random
+
+
+# import matplotlib.pyplot as pl
+# from matplotlib.ticker import MultipleLocator
+
+
+def flip_coin():
+    result = {i: 0 for i in range(11)}
+
+    for _ in range(10000):
+        result[sum([random.randint(0, 1) for _ in range(10)])] += 1
+
+    return {key: (value / 100) for key, value in result.items()}
