90260530baab7039517e16b06d45d2b4cefa5dce
bohdan <bodyan1108@gmail.com>
2022-06-07 16:46:25 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import random
+
+
+def flip_coin():
+    result = {j: 0 for j in range(11)}
+
+    for _ in range(10000):
+        hits = 0
+
+        for _ in range(10):
+            choice = random.choice(["eagle", "tails"])
+            if choice == "eagle":
+                hits += 1
+
+        result[hits] += 1
+
+    return {key: value / 100 for key, value in result.items()}
