83e33064990fd6ae1179c8c5148700f626309dc3
Yurnerroo <yura.zdanevych@gmail.com>
2022-06-08 13:50:09 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ import random
 def flip_coin():
     result = {i: 0 for i in range(11)}
 
-    for i in range(10000):
+    for _ in range(10000):
         flip = 0
 
-        for j in range(10):
+        for _ in range(10):
             face = random.randint(0, 1)
             if face:
                 flip += 1
