7b6ae31d87a26eaf6f88a25d4a560ab20f366a82
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2022-06-08 20:55:53 +0300

    'TheFix'

--- app/main.py
+++ app/main.py
@@ -2,36 +2,17 @@ import random
 
 
 def flip_coin():
-    count = 11
+    count = 0
     final_dict = {i: 0 for i in range(11)}
     list_ = ["heads", "tails"]
     for i in range(10000):
-        if count == 0:
-            final_dict[0] += 1
-        if count == 1:
-            final_dict[1] += 1
-        if count == 2:
-            final_dict[2] += 1
-        if count == 3:
-            final_dict[3] += 1
-        if count == 4:
-            final_dict[4] += 1
-        if count == 5:
-            final_dict[5] += 1
-        if count == 6:
-            final_dict[6] += 1
-        if count == 7:
-            final_dict[7] += 1
-        if count == 8:
-            final_dict[8] += 1
-        if count == 9:
-            final_dict[9] += 1
-        if count == 10:
-            final_dict[10] += 1
-        count = 0
         for i in range(10):
             item = random.choice(list_)
             if item == "heads":
                 count += 1
+        final_dict[count] += 0.01
+        count = 0
+    for i in range(11):
+        rounded_number = round(final_dict[i], 2)
+        final_dict[i] = rounded_number
     return final_dict
-
