89811acd0ddbb03df967d742bd654725abeeff78
Anton Lasko <anton.lasko1993@gmail.com>
2022-06-17 18:00:35 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
-# write your code here
+import random
+
+dictionary = {
+    1: 0.0,
+    2: 0.0,
+    3: 0.0,
+    4: 0.0,
+    5: 0.0,
+    6: 0.0,
+    7: 0.0,
+    8: 0.0,
+    9: 0.0,
+    10: 0.0}
+option = ["heads", "tails"]
+
+
+def flip_coin():
+    for case in range(0, 10000):
+        for num, flip in enumerate(range(1, 11)):
+            flip = random.choice(option)
+            if flip == "heads":
+                dictionary[num] += 1.0
+
+    for value in dictionary:
+        dictionary[value] = (dictionary[value] / 1000) * 100
+    return dictionary
