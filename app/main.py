137eb2704c191fd023b5597e5ebc702d8814d4ff
Anton Lasko <anton.lasko1993@gmail.com>
2022-06-18 15:49:03 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,16 +1,8 @@
 import random
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
-dictionary = {
-    1: 0.0,
-    2: 0.0,
-    3: 0.0,
-    4: 0.0,
-    5: 0.0,
-    6: 0.0,
-    7: 0.0,
-    8: 0.0,
-    9: 0.0,
-    10: 0.0}
+dictionary = {}
 option = ["heads", "tails"]
 
 
@@ -19,8 +11,23 @@ def flip_coin():
         for num, flip in enumerate(range(1, 11)):
             flip = random.choice(option)
             if flip == "heads":
-                dictionary[num] += 1.0
+                if dictionary[num] in dictionary.keys():
+                    dictionary[num] += 1.0
+                else:
+                    dictionary[num] = 1
 
     for value in dictionary:
         dictionary[value] = (dictionary[value] / 1000) * 100
     return dictionary
+
+
+def draw_gaussian_distribution_graph():
+    my_dictionary = flip_coin()
+    mpl.rcParams.update({'font.size': 10})
+    plt.axis([0, 10, 0, 100])
+    plt.title('Gaussian distribution')
+    plt.xlabel('Heads count')
+    plt.ylabel('Drop percentage %')
+    x_axis = list(my_dictionary.keys())
+    y_axis = list(my_dictionary.values())
+    plt.plot(x_axis, y_axis, color='blue', linestyle='solid')
