8cf83bbfcdbe86b5c334d94bfc370c46386d85f5
Dmytro Hushchyk <107995555+DHushchyk@users.noreply.github.com>
2022-07-21 18:17:58 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,15 +12,3 @@ def flip_coin():
         )
         results[i] = count / 100
     return results
-
-
-def draw_gaussian_distribution_graph():
-    x = [key for key in flip_coin().keys()]
-    y = [value for value in flip_coin().values()]
-    pyplot.plot(x, y, "r")
-    pyplot.ylim(0, 100)
-    pyplot.xlim(0, 10)
-    pyplot.title("Gaussian distribution", fontsize=20)
-    pyplot.xlabel("Heads count", fontsize=16)
-    pyplot.ylabel("Drop percentage %", fontsize=16)
-    pyplot.show()
