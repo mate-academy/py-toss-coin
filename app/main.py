299750091496bb714eb20ae61fa7eba4d9c27b08
Andrii Padalko <a.padalko@gmail.com>
2022-07-31 18:59:34 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import random
+
+
+def flip_coin():
+
+    list_of_total_cases = [
+        [random.choice(["h_s", "t_s"]) for _ in range(10)] for _ in range(1000)
+    ]
+
+    list_of_count_heads = [case.count("h_s") for case in list_of_total_cases]
+
+    dict_count_heads = {i: list_of_count_heads.count(i) / 10
+                        for i in range(11)}
+
+    return dict_count_heads
