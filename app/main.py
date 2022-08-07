a5868f71af316c80b1a18974337b411a8f67624a
stoveprofi <stoveprofi@gmail.com>
2022-08-07 17:23:17 +0100

    Corrected

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 
 
 def flip_coin():
-    total = [[random.choice(['c', 'h']) for _ in range(10)] for _ in range(10000)]
+    total = [[random.choice(['c', 'h'])
+              for _ in range(10)] for _ in range(10000)]
     list_count = [case.count('h') for case in total]
     dicts = {i: list_count.count(i) / 100 for i in range(11)}
     return dicts
@@ -16,10 +17,8 @@ def draw_gaussian_distribution_graph():
         x.append(key)
         y.append(value)
     plt.figure(figsize=(12, 7))
-    plt.plot(x, y, 'o-r', alpha=0.7, label="first", lw=5, mec='b', mew=2, ms=10)
+    plt.plot(x, y, 'o-r', alpha=0.7, label="first",
+             lw=5, mec='b', mew=2, ms=10)
     plt.legend()
     plt.grid(True)
     plt.show()
-
-
-#print(draw_gaussian_distribution_graph())
