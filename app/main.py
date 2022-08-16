b11b5fb217a31895dd093ef13fc71ce4f476bb7c
AnDy <ansmbox@gmail.com>
2022-08-16 22:28:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import random
+
+
+def flip_coin():
+    result = {}.fromkeys(range(11), 0)
+    count_drops = 10000
+
+    for i in range(count_drops):
+        key = round(random.gauss(5, 1.6))
+        if 0 <= key <= 10:
+            result[key] += 1
+
+    for key in result:
+        result[key] = result[key] * 100 / 10000
+
+    return result
+
+
+print(flip_coin())
