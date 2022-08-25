6ffea384f8c1b6bf97e7616b443c96ceb788613e
Anastasia Martyniuk <anastasia.20.12.1995@gmail.com>
2022-08-25 14:47:17 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,30 @@
-# write your code here
+import math
+import random
+
+
+def factorial(n: int):
+    if n == 0:
+        return 1
+    else:
+        return math.prod([a for a in range(1, n + 1)])
+
+
+def flip_coin() -> dict:
+    head = 0
+    flip = 0
+    result = {}
+    for _ in range(0, 10_000):
+        z = random.randint(0, 1)
+        if z == 1:
+            head += 1
+        else:
+            flip += 1
+
+    head /= 10_000
+    flip /= 10_000
+
+    for one_flip in range(0, 11):
+        y = factorial(10) / (factorial(one_flip) * factorial(10 - one_flip))
+        x = y * (head ** one_flip) * (flip ** (10 - one_flip))
+        result.update({one_flip: x * 100})
+    return result
