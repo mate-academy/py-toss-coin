956a88be7d8510ab66a1c6929be84609227dc075
Paul Starodub <amorallex@gmail.com>
2022-09-14 09:10:52 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 import random
 
+import matplotlib.pyplot as plt
+
 
 def flip_coin():
     dict_ = {0: 0,
@@ -14,9 +16,19 @@ def flip_coin():
              9: 0,
              10: 0,
              }
+    coin = ["head", "tail"]
     for i in range(10000):
         count = 0
         for j in range(10):
-            count += random.randint(0, 1)
-        dict_[count] = round(dict_[count] + 0.01)
+            count += 1 if random.choice(coin) == "head" else 0
+        dict_[count] += 0.01
     return dict_
+
+
+def draw_gaussian_distribution_graph(dict_: dict):
+    plt.plot(dict_.keys(), dict_.values())
+    plt.ylim(0, 100)
+    plt.title("Gaussian_distribution")
+    plt.xlabel("Count_of_head")
+    plt.ylabel("Drop percentage")
+    plt.show()
