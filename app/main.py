dbe8b46d9a1d0822559ebc0827087a92c34bcc83
Foggy-Cat <rosky.yaroslav@gmail.com>
2022-09-17 21:24:25 +0300

    Solut

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def flip_coin():
     coin_dict = {i: 0 for i in range(11)}
     for j in range(10000):
         count = 0
-        for j in range(10):
+        for _ in range(10):
             if random.randint(1, 2) == 1:
                 count += 1
         coin_dict[count] += 1
