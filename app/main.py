9a0963f1fc94a45424b147944cb8ab60ce070473
Eugene <buntyc8@gmail.com>
2022-09-26 18:21:32 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import random
+
+
+def flip_coin():
+    throw_count = []
+
+    for _ in range(10000):
+        round = 0
+
+        for i in range(10):
+            if random.randint(1, 2) == 1:
+                round += 1
+        throw_count.append(round)
+
+    result = {x: (throw_count.count(x) / (len(throw_count) / 100))
+              for x in range(11)}
+    return result
