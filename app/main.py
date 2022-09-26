1b849e7bda9416bc1d616b0efbd592534246145b
Egor Voloshyn <103663723+AriohBelskij@users.noreply.github.com>
2022-09-26 09:12:40 +0300

    Seblution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    seb = [i for i in range(11)]
+    seb = {i: 0 for i in seb}
+    variants = ["heads", "tails"]
+    for _ in range(10000):
+        count = 0
+        for _ in range(10):
+            if random.choice(variants) == "heads":
+                count += 1
+        seb[count] += 1
+    seb = {i: v / 100 for i, v in seb.items()}
+    return seb
+
+
+print(flip_coin())
