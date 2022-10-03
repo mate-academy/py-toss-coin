55dfeed2639b8a3df31c0c73bdbb174004a1675a
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-10-03 12:36:09 +0300

    Solution for the task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import random
+
+
+def flip_coin():
+    dictionary = {i: 0 for i in range(11)}
+
+    for _ in range(10000):
+        count = 0
+
+        for _ in range(10):
+            count += random.randint(0, 1)
+
+        dictionary[count] += 1
+
+    for i in range(11):
+        dictionary[i] /= 100
+
+    return dictionary
