f0a1b5ba7d8326b3447d61277c528d4a467109f5
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-10-03 23:06:02 +0300

    Added graph

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
 import random
+import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib.pyplot import MultipleLocator
+matplotlib.use('TkAgg')
 
 
 def flip_coin():
@@ -16,3 +20,28 @@ def flip_coin():
         dictionary[i] /= 100
 
     return dictionary
+
+
+def draw_gaussian_distribution_graph():
+    dictionary = flip_coin()
+
+    xpoints = list(dictionary.keys())
+    ypoints = list(dictionary.values())
+
+    plt.axis([0, 10, 0, 100])
+
+    x_major_locator = MultipleLocator(1)
+    y_major_locator = MultipleLocator(10)
+    ax = plt.gca()
+    ax.xaxis.set_major_locator(x_major_locator)
+    ax.yaxis.set_major_locator(y_major_locator)
+    plt.xlim(0, 10)
+    plt.ylim(0, 100)
+
+    plt.plot(xpoints, ypoints)
+
+    plt.title("Gaussian distribution")
+    plt.xlabel("Heads count")
+    plt.ylabel("Drop percentage %")
+
+    plt.show()
