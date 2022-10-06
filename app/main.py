777d99d08eb54fdc7505e249e0052286d974f102
Zakhar Podporinov <zp162162@gmail.com>
2022-10-06 03:18:30 +0300

    Complete!

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    flips_rate = {number: 0 for number in range(11)}
+
+    for _ in range(10000):
+        count = 0
+        for _ in range(10):
+            if random.choice(["Head", "Tail"]) == "Head":
+                count += 1
+
+        flips_rate[count] += 1
+
+    return {key: value / 100 for key, value in flips_rate.items()}
