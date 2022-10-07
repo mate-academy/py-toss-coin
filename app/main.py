2db165b87fd50f3160adcf3a5aa21ae48ef0eb7b
graandda <graanddadays@gmail.com>
2022-10-07 19:27:12 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+
+    flips = [[random.randint(0, 1)
+              for _ in range(10)].count(1)
+             for _ in range(10000)]
+    return {time: round(flips.count(time) * 0.01, 2) for time in range(0, 11)}
