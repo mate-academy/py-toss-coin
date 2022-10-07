b087fde08b9bb1c84917d6df91fdd464be7e30b9
Illia Artnaiev <ilya.artnaev@gmail.com>
2022-10-07 15:45:17 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    times = 10000
+    coins_flipped = {x: 0 for x in range(11)}
+
+    for _ in range(times):
+        heads = 0
+
+        for _ in range(10):
+            heads += random.randint(0, 1)
+
+        coins_flipped[heads] += 1
+
+    result = {
+        count: values / times * 100 for count, values in coins_flipped.items()
+    }
+
+    return result
