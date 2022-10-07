68692259c34deca5a5628aa08143d29e2b01695c
Roman Kolmakov <cerne313@gmail.com>
2022-10-07 19:54:13 +0300

    Solve the task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    res = {key: 0 for key in range(11)}
+
+    for _ in range(10000):
+        head_count = 0
+
+        for _ in range(10):
+            if random.randint(1, 2) == 1:
+                head_count += 1
+        res[head_count] += 1
+
+    return {key: value / 100 for key, value in res.items()}
