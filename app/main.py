9d12f1d73888167a19891856565b71683d163ab6
Dima Ursatii <Dimon4ir@gmail.com>
2022-10-07 15:15:19 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import random
+from collections import Counter
+
+
+def flip_coin():
+    result = {key: 0 for key in range(0, 11)}
+    for _ in range(10000):
+        count = 0
+        for _ in range(0, 10):
+            count += random.randint(0, 1)
+        result[count] += 1
+
+    for i in result:
+        result[i] /= 100
+    return result
