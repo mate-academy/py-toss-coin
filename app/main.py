b9ddf5b80fed1be0f6194e9d0aae41217930c7c3
Dmytro Petliovanyi <petliovany@gmail.com>
2022-10-09 15:45:23 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    distribution = {i: 0 for i in range(11)}
+    for _ in range(0, 10000):
+        count = 0
+
+        for _ in range(0, 10):
+            flip = random.randint(0, 1)
+
+            if flip == 0:
+                count += 1
+
+        distribution[count] += 1
+
+    for i in range(11):
+        distribution[i] /= 100
+
+    return distribution
