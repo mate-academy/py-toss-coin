da4974d52fda6c7f0da3f3d34532a4b40217ac8a
Dima Ovcharenko <kukuku381@gmail.com>
2022-10-09 11:41:41 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    coin_flip = {i: 0 for i in range(11)}
+    range_coin = 10_000
+
+    for _ in range(range_coin):
+        counter = 0
+
+        for _ in range(10):
+            counter += random.randint(0, 1)
+        coin_flip[counter] += 1
+
+    coin_flip = {k: v / range_coin * 100 for k, v in coin_flip.items()}
+
+    return coin_flip
