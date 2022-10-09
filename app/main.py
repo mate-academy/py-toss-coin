2ab309575f7e488893b8d863b4f66448f89adab9
Pavlo Kapitanets <aixpavlik"gmail.com>
2022-10-09 09:59:46 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -14,22 +14,12 @@ def count_heads() -> int:
 
 
 def flip_coin() -> dict:
-    count_flips = {0: 0,
-                   1: 0,
-                   2: 0,
-                   3: 0,
-                   4: 0,
-                   5: 0,
-                   6: 0,
-                   7: 0,
-                   8: 0,
-                   9: 0,
-                   10: 0}
+    count_flips = {key: 0 for key in range(11)}
     for number in range(10000):
         count_flips[count_heads()] += 1
 
     percentage_count_flips = {}
     for number in range(11):
         percentage_count_flips[number] = (
-            round((count_flips[number] / 10000 * 100), 2))
+            round((count_flips[number] / 100), 2))
     return percentage_count_flips
