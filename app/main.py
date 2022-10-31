9c5eb04612d949584815d530230649e3f4f5bc97
George <georgepavlej@gmail.com>
2022-10-31 23:33:59 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+import numpy as np
+
+
+def flip_coin() -> dict:
+    flips = list(np.random.binomial(n=10, p=0.5, size=10000))
+    return dict(zip([i for i in range(11)], [(np.equal(flips, i).sum() * 100)
+                                             / 10000 for i in range(11)]))
