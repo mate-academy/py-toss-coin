6af6506512cc9100ba48514d8786af8d4b3db51f
Taras Zelinskyi <taras.sv89@gmail.com>
2022-11-04 01:03:34 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,42 @@
-# write your code here
+import random
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+
+def flip_coin() -> dict:
+    result_dict = {}
+    for _ in range(10000):
+        count = 0
+        for i in range(10):
+            x_side = random.randint(0, 1)
+            if x_side == 1:
+                count += 1
+        if count in result_dict:
+            result_dict[count] += 1
+        else:
+            result_dict[count] = 1
+    keys = sorted(result_dict.items())
+    sorted_dict = {}
+    for key, value in keys:
+        sorted_dict[key] = (value / 100)
+    print(sorted_dict)
+    return sorted_dict
+
+
+def draw_gaussian_distribution_graph(data: dict) -> None:
+    mpl.rcParams.update({"font.size": 10})
+    plt.axis([0, 10, 0, 100])
+
+    fig, ax = plt.subplots()
+    x_axis = data.keys()
+    y_axis = data.values()
+    plt.plot(x_axis, y_axis, color="blue", linestyle="solid")
+
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(5))
+
+    plt.title("Gaussian distribution")
+    plt.xlabel("Heads count")
+    plt.ylabel("Drop percentage %")
+    fig.savefig("Gaussian_distribution.png")
