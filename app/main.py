5ac33e8dc9aa99ef059a4f398cad59d0bf24422a
Andrey Losev <andry07070712@gmail.comless.gitconfiggit config --global user.email andry07070712@gmail.com>
2022-11-07 12:24:42 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    for _ in range(20):
+        return {0: random.uniform(0.01, 0.6),
+                1: random.uniform(0.6, 2),
+                2: random.randint(2, 10),
+                3: random.randint(10, 18),
+                4: random.randint(18, 22),
+                5: random.randint(22, 27),
+                6: random.randint(18, 22),
+                7: random.randint(10, 18),
+                8: random.randint(2, 10),
+                9: random.uniform(0.6, 2),
+                10: random.uniform(0.01, 0.6)}
