d142bb45798063cd95b620dd12061a0cda3d8ba2
Dede <specialized8393@gmail.com>
2022-11-10 12:30:12 +0000

    Writed flip_coin function with 10000 cases

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import matplotlib
+import random
+
+
+def flip_coin():
+    coins = {}
+    for _ in range(10000):
+        count = 0
+        for i in range(10):
+            if random.choice(["head", "tails"]) == "head":
+                count += 1
+        if count in coins:
+            coins[count] += 1
+        else:
+            coins[count] = 1
+
+    for key in coins:
+        coins[key] = round((coins[key] / 10000) * 100, 2)
+    return coins
+
