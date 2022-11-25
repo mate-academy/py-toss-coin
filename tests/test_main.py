44e2a746e26bf60ba8fdba90bf3551b468e03b9c
Admin <winofkill@gmail.com>
2022-11-25 14:46:57 +0200

    Solution

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,9 +4,9 @@ from app.main import flip_coin
 
 
 def test_func_should_return_dict():
-    assert isinstance(flip_coin(), dict), (
-        "Function 'flip_coin' should return dictionary"
-    )
+    assert isinstance(
+        flip_coin(), dict
+    ), "Function 'flip_coin' should return dictionary"
 
 
 def test_function_should_return_different_values():
@@ -17,8 +17,7 @@ def test_function_should_return_different_values():
         cache.add(tuple(result for result in coins.values()))
 
     assert len(cache) == 20, (
-        "Function should return different values, "
-        "because 'random' should be used"
+        "Function should return different values, " "because 'random' should be used"
     )
 
 
@@ -30,62 +29,22 @@ def test_function_should_return_different_values():
             22,
             27,
         ),
-        pytest.param(
-            4,
-            18,
-            22
-        ),
-        pytest.param(
-            6,
-            18,
-            22
-        ),
-        pytest.param(
-            3,
-            10,
-            18
-        ),
-        pytest.param(
-            7,
-            10,
-            18
-        ),
-        pytest.param(
-            2,
-            2,
-            10
-        ),
-        pytest.param(
-            8,
-            2,
-            10
-        ),
-        pytest.param(
-            1,
-            0.6,
-            2
-        ),
-        pytest.param(
-            9,
-            0.6,
-            2
-        ),
-        pytest.param(
-            10,
-            0.01,
-            0.6
-        ),
-        pytest.param(
-            0,
-            0.01,
-            0.6
-        ),
+        pytest.param(4, 18, 22),
+        pytest.param(6, 18, 22),
+        pytest.param(3, 10, 18),
+        pytest.param(7, 10, 18),
+        pytest.param(2, 2, 10),
+        pytest.param(8, 2, 10),
+        pytest.param(1, 0.6, 2),
+        pytest.param(9, 0.6, 2),
+        pytest.param(10, 0.01, 0.6),
+        pytest.param(0, 0.01, 0.6),
     ],
 )
 def test_gausian_distribution(number, expected, limit):
     for _ in range(20):
         coins = flip_coin()
 
-        assert expected <= coins[number] <= limit, (
-            f"There must be > {number}% of '{expected}' value"
-        )
+        assert (
+            expected <= coins[number] <= limit
+        ), f"There must be > {number}% of '{expected}' value"
