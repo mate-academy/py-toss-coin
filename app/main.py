9c5a3e35307fb281bd37e72828d588064ce66ae7
nazar-dmytrenko <nazarkadm@mail.com>
2023-01-11 15:32:05 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import random
+
+
+def flip_coin(number: int = 10000) -> dict:
+
+    drops = ["Eagle", "Tails"]
+    dropout_result = []
+
+    for round_throws in range(number):
+
+        count_tails = 0
+
+        for toss in range(10):
+            if random.choice(drops) == "Tails":
+                count_tails += 1
+        dropout_result.append(count_tails)
+
+    return {
+        x: dropout_result.count(x) / number * 100 for x in range(0, 11)
+    }
