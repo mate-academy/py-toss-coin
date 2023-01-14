4e2dfd5d9d0ebd5e00305e702bb0debf7c65b7c1
саша1 <magazynuniversam@gmail.com>
2023-01-14 15:30:25 +0200

    sol 4

--- app/main.py
+++ app/main.py
@@ -1,40 +1,20 @@
 import random
-import matplotlib.pyplot as plt
 
 
-def flip_coin() -> dict:
-    coin = (0, 1)
-    result_dict = {0: 0,
-                   1: 0,
-                   2: 0,
-                   3: 0,
-                   4: 0,
-                   5: 0,
-                   6: 0,
-                   7: 0,
-                   8: 0,
-                   9: 0,
-                   10: 0
-                   }
+def flip_coin(number: int = 10000) -> dict:
 
-    for _ in range(10000):
-        num_of_heads = [random.choice(coin) for _ in range(10)].count(0)
-        result_dict[num_of_heads] += 0.01
+    drops = ["Eagle", "Tails"]
+    dropout_result = []
 
-    return result_dict
+    for round_throws in range(number):
 
+        count_tails = 0
 
-def draw_gaussian_distribution_graph(result_dict: dict) -> None:
-    x_points = result_dict.keys()
-    y_points = result_dict.values()
+        for toss in range(10):
+            if random.choice(drops) == "Tails":
+                count_tails += 1
+        dropout_result.append(count_tails)
 
-    plt.plot(x_points, y_points)
-    plt.title("Gaussian distributeion")
-    plt.xlabel("Heads count")
-    plt.ylabel("Drop percentage %")
-
-    plt.grid()
-    plt.show()
-
-
-draw_gaussian_distribution_graph(flip_coin())
+    return {
+        x: dropout_result.count(x) / number * 100 for x in range(0, 11)
+    }
