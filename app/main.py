8e63a257e27845f760b10e9c0a21ed04e7262006
unknown <you@example.com>
2023-01-20 21:48:42 +0200

    Test

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
 # write your code here
+import random
+
+
+def flip_coin():
+    total_count_heads = 0
+    total_dict = {}
+    num = 0
+    for i in range(5):
+        heads_count = 0
+        for k in range(10):
+            heads_count += random.randint(0, 1)
+        print("Head count = ", heads_count)
+        total_count_heads += heads_count
+        print("Total count = ", total_count_heads)
+        total_dict[heads_count] = total_dict.get(heads_count, 0) + 1
+    return total_dict
+
+
+print(flip_coin())
