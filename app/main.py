651c6e9b20a5921afb132c817e49b97da824b4ae
unknown <you@example.com>
2023-01-20 23:15:38 +0200

    Draft_2

--- app/main.py
+++ app/main.py
@@ -6,16 +6,16 @@ def flip_coin():
     total_count_heads = 0
     total_dict = {}
     super_total_dict = {}
-    for i in range(5):
+    for i in range(10000):
         heads_count = 0
         for k in range(10):
             heads_count += random.randint(0, 1)
         total_count_heads += heads_count
-        print("Total count = ", total_count_heads)
         total_dict[heads_count] = (total_dict.get(heads_count, 0) + 1)
-        print(total_dict)
     for key, value in total_dict.items():
         super_total_dict[key] = round(value / total_count_heads, 3)
+    print(super_total_dict)
+    super_total_dict = dict(sorted(super_total_dict.items()))
     return super_total_dict
 
 
