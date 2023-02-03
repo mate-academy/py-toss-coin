b288f0a25546179e1066bf62bb75ece52591f89e
Vitalii Khmura <vitalikkhmura13@gmail.comw>
2023-02-03 23:04:41 +0200

    Changed Solution

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import random
 import matplotlib.pyplot as plt
 
 
-def flip_coin() -> list:
+def flip_coin() -> dict:
     coin = ["head", "tail"]
     probability = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0,
                    8: 0, 9: 0, 10: 0}
@@ -15,17 +15,4 @@ def flip_coin() -> list:
         probability[result] += 1
     for key in probability.keys():
         probability[key] /= 100
-
     return probability
-
-
-def draw_gaussian_distribution_graph() -> None:
-    points = []
-    for key in flip_coin().values():
-        points.append(key)
-    plt.plot(points)
-    plt.title("Gaussian distribution")
-    plt.xlabel("Head count")
-    plt.ylabel("Drop percentage %")
-
-    plt.show()
