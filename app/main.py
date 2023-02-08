926cfdc10bfb0554195e31251a94201266db5089
Yehor Kornev <dmekkindom00@gmail.com>
2023-02-08 21:14:15 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,25 +5,36 @@ import matplotlib.pyplot as plt
 
 def flip_coin() -> dict:
     possible_heads_dropped = {}
-    for index in range(11):
-        possible_heads_dropped[index] = sum([random.randint(0, 1) for _ in range(10000)]) / 100
+    heads_drop = {}
+
+    for _ in range(10000):
+        drop_heads = sum([random.randint(0, 1) for _ in range(10)])
+
+        if heads_drop.get(drop_heads):
+            heads_drop[drop_heads] += 1
+        else:
+            heads_drop[drop_heads] = 1
+
+    for number, possible_drop in heads_drop.items():
+        possible_heads_dropped[number] = possible_drop / 100
 
     return possible_heads_dropped
 
 
 def draw_gaussian_distribution_graph() -> None:
+    flip_coin_dict = flip_coin()
 
-    x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
-    y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+    xpoint = np.array(
+        [heads_count for heads_count in flip_coin_dict]
+    )
+    ypoint = np.array(
+        [drop_percentage for drop_percentage in flip_coin_dict.values()]
+    )
 
-    plt.plot(x, y)
+    plt.plot(xpoint, ypoint)
 
-    plt.title("Sports Watch Data")
-    plt.xlabel("Average Pulse")
-    plt.ylabel("Calorie Burnage")
+    plt.title("Gaussian distribution")
+    plt.xlabel("Heads count")
+    plt.ylabel("Drop percentage %")
 
     plt.show()
-
-
-print(flip_coin())
-print(draw_gaussian_distribution_graph())
