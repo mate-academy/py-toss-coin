ec0d79b26ad0acae5ee47bdef12f5d3176050fe5
Vyacheslav Kungin <acheros4@gmail.com>
2023-02-14 14:19:43 +0200

    add_grphs

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 import random
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def flip_coin() -> dict:
@@ -15,3 +17,15 @@ def flip_coin() -> dict:
         result[count_1] = round(result[count_1] + 0.01, 2)
 
     return result
+
+
+def draw_gaussian_distribution_graph(date: dict) -> None:
+    x_points = np.array(list(date.keys()))
+    y_points = np.array(list(date.values()))
+
+    plt.plot(x_points, y_points)
+
+    plt.title("Head count")
+    plt.xlabel("Number of heads up")
+    plt.ylabel("Percentage of 10,000 attempts")
+    plt.show()
