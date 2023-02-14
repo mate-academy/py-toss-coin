05c75fec266f12d76b64f7055789df3cbb6679ab
Vyacheslav Kungin <acheros4@gmail.com>
2023-02-14 13:43:58 +0200

    flip_coin

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    result = {
+        0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0
+    }
+    coin_sides = [0, 1]
+
+    for _ in range(10000):
+        count_1 = 0
+        for _ in range(10):
+            if random.choice(coin_sides) == 1:
+                count_1 += 1
+        result[count_1] = round(result[count_1] + 0.01, 2)
+
+    return result
