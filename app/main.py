7a8218ba198bac5d20c55f7b3cfee517955e9e3f
David Gluschenko <davidglusch@gmail.com>
2023-02-22 14:19:57 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import numpy as np
+
+
+def flip_coin() -> dict:
+    results = {}
+    flips = 10000
+    for i in range(11):
+        results[i] = 0
+    for i in range(flips):
+        heads = np.random.binomial(10, 0.5)
+        results[heads] += 1
+    for key in results:
+        results[key] = round((results[key] / flips) * 100, 2)
+    return results
