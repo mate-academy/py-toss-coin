f1ce5e799a9d1308cc736f8165387bc4130fee48
Eugene <eugene.lot77@gmail.com>
2023-02-24 18:45:06 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import random
+
+
+def flip_coin() -> None:
+    results = {}
+    cases = 10000
+
+    for i in range(cases):
+        flips = [random.choice(["Eagle", "Tail"]) for _ in range(10)]
+        eagle = flips.count("Eagle")
+
+        if eagle not in results:
+            results[eagle] = 0
+        results[eagle] += 1
+
+    for key in results:
+        results[key] = (results[key] / cases) * 100
+
+    return results
