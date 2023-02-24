b24af9d07d53607ff6e6b8935a470af3cfbb05d0
ramsty <ramsty27@gmail.com>
2023-02-24 18:42:43 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,9 +5,9 @@ def flip_coin() -> dict:
     results = {}
     num_trials = 10000
 
-    for i in range(num_trials):
+    for _ in range(num_trials):
         num_heads = 0
-        for j in range(10):
+        for _ in range(10):
             if random() < 0.5:
                 num_heads += 1
         if num_heads in results:
@@ -15,7 +15,7 @@ def flip_coin() -> dict:
         else:
             results[num_heads] = 1
 
-    for k in results:
-        results[k] = round(results[k] / num_trials * 100, 2)
+    for num in results:
+        results[num] = round(results[num] / num_trials * 100, 2)
 
     return results
