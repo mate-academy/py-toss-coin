7fc5cfa6e8b1da45cc6bc8fb7d836f0313c84edd
drapail <mv.dibrov@gmail.com>
2023-02-24 22:12:31 +0100

    increased number of interatios

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ import random
 
 def flip_coin() -> dict:
     dictionary = {x: 0 for x in range(11)}
-    for i in range(20000):
+    for i in range(30000):
         total_heads = 0
         for flip_number in range(10):
             total_heads += random.randint(0, 1)
         dictionary[total_heads] += 1
-    return {key: round((value / 20000), 3) * 100 for key, value in
+    return {key: round((value / 30000), 3) * 100 for key, value in
             dictionary.items()}
