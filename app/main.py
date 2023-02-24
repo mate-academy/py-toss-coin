e8f2503665b302c8fd06c56bf1b6b4acc30ee786
Anatolii Pozniak <a13101990a@gmail.com>
2023-02-24 18:07:54 +0200

    Solution without creating a dict template

--- app/main.py
+++ app/main.py
@@ -6,30 +6,24 @@ from matplotlib.ticker import AutoMinorLocator
 
 
 def flip_coin() -> dict:
-    result_dict = {
-        0: 0,
-        1: 0,
-        2: 0,
-        3: 0,
-        4: 0,
-        5: 0,
-        6: 0,
-        7: 0,
-        8: 0,
-        9: 0,
-        10: 0
-    }
+    result_dict = {}
     coin = ["Heads", "Tails"]
     for case in range(10000):
         count = 0
         for flip in range(10):
             if random.choice(coin) == "Heads":
                 count += 1
-        result_dict[count] += 1
+        if count in result_dict:
+            result_dict[count] += 1
+            continue
+        result_dict[count] = count
     for num_of_heads_drop, times_of_drop in result_dict.items():
         percent_of_drop = times_of_drop / 100
         result_dict[num_of_heads_drop] = percent_of_drop
-    return result_dict
+    return {
+        count_of_heads: result_dict[count_of_heads] for count_of_heads in
+        sorted(sorted_keys for sorted_keys in result_dict.keys())
+    }
 
 
 def draw_gaussian_distribution_graph(result_dict: dict[flip_coin()]) -> None:
