03c25f87c22d0e79b815e3f149d9bf42592297f8
MikhailLyvak <michael.lyvak@gmail.com>
2023-02-24 13:45:32 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+from random import choice
+
+
+def flip_coin() -> dict:
+    statistic = {}
+    to_choice = ["head", "tail"]
+
+    for _ in range(10000):
+        res = [choice(to_choice) for _ in range(10)].count("head")
+        if statistic.get(res):
+            statistic[res] += 1
+        else:
+            statistic[res] = 1
+
+    return {key: val / 100 for key, val in statistic.items()}
