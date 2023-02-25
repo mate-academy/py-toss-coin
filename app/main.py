1ad8bef5389db96f68e5c279565a8fbd0fd9b20c
Bohdan Lazaryshyn <lazaryshyn1998@gmail.com>
2023-02-25 19:14:32 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,20 +2,16 @@ import random
 
 
 def flip_coin() -> dict:
-    result_dict = {}
+    result_dict = {i: 0 for i in range(11)}
     result_ls = []
 
-    for i in range(1000):
+    for i in range(10000):
         result_flip = 0
         for _ in range(10):
-            flip = random.randint(0, 1)
-            if flip == 0:
-                result_flip += 1
+            result_flip += random.randint(0, 1)
         result_ls.append(result_flip)
 
-    result_ls = sorted(result_ls)
     for result in result_ls:
-        if result not in result_dict:
-            result_dict[result] = round(result_ls.count(result)
-                                        / 1000 * 100, 2)
+        result_dict[result] = round(result_ls.count(result)
+                                    / 10000 * 100, 2)
     return result_dict
