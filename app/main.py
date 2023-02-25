f6c9e1a5aa420632deb696dc00811357279c1cf1
Artur Tytomyr <art.tytomyr@gmail.com>
2023-02-25 20:18:35 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+from random import randint
+from matplotlib import pyplot
+
+
+def flip_coin() -> dict:
+    flips = {i: 0 for i in range(11)}
+    for i in range(10000):
+        heads = 0
+        for _ in range(10):
+            if randint(0, 1) == 1:
+                heads += 1
+        flips[heads] += 1
+    return {key: value / 100 for key, value in flips.items()}
+
+
+def draw_gaussian_distribution_graph(data: dict) -> None:
+    pyplot.plot(data.keys(), data.values())
+    pyplot.show()
