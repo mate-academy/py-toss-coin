48eeaf541830146544721b9ee562655c00164ab3
Nazar Hladaniuk <powerlolek46@gmail.com>
2023-02-25 11:57:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,7 +5,9 @@ import numpy as np
 
 def flip_coin() -> dict:
     try_num = 0
-    count_dict = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
+    count_dict = {
+        0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0
+    }
 
     for i in range(10000):
 
@@ -15,13 +17,13 @@ def flip_coin() -> dict:
             count_rand += random_number
         count_dict[count_rand] += 1
 
-    for k, v in count_dict.items():
-        count_dict[k] = (v / 100)
+    for key, value in count_dict.items():
+        count_dict[key] = (value / 100)
 
     return count_dict
 
 
-def draw_gaussian_distribution_graph(count_dict) -> None:
+def draw_gaussian_distribution_graph(count_dict: dict) -> None:
     xpoints = np.array(list(count_dict.keys()))
     ypoints = np.array(list(count_dict.values()))
 
