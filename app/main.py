79f0ffe44b642ebd638635e741d44f6fda172be3
oithegod <serejathegod@gmail.com>
2023-02-26 16:17:45 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import random
+
+
+def ten_trows() -> list:
+    return [random.randint(0, 1) for _ in range(10)]
+
+
+def flip_coin() -> dict:
+    result_dict = {k: 0 for k in range(11)}
+
+    for _ in range(10000):
+        result_dict[ten_trows().count(1)] += 1 / 100
+
+    return result_dict
