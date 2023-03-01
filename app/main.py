ddd2c1a11f302c4591459e348ba20c72707f3635
Maksym_Sylantiev <maxym2s@yahoo.com>
2023-03-01 15:07:52 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import random
+from matplotlib import pyplot
+
+
+def ten_trows() -> list:
+    return [random.randint(0, 1) for _ in range(10)]
+
+
+def flip_coin() -> dict:
+    result_dict = {k: 0 for k in range(11)}
+
+    for _ in range(10000):
+        result_dict[ten_trows().count(1)] += 1 / 100
+
+    return result_dict
+
+
+def draw_gaussian_distribution_graph(data: dict) -> None:
+    pyplot.plot(data.keys(), data.values())
+    pyplot.show()
