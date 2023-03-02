23f694cac09621fb450b16f6be4b5e1140dc94c9
Oleh Chornyi <ochernyi04@gmail.com>
2023-03-02 16:20:10 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    result = {
+        0: 0, 1: 0, 2: 0, 3: 0, 4: 0,
+        5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0
+    }
+    for case in range(10000):
+        dropping = 0
+        side = ["heads", "opposite"]
+        for i in range(10):
+            drop = random.choice(side)
+            if drop == "heads":
+                dropping += 1
+
+        result[dropping] += 1
+    for key, value in result.items():
+        result[key] = value / 100
+
+    return result
