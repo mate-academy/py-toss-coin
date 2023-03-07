8d56cbe99ac35bb865fa5587e0bc96cc6a4bf61b
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-03-07 14:07:04 +0200

    Solution test of approval

--- app/main.py
+++ app/main.py
@@ -2,19 +2,19 @@ import random
 
 
 def flip_coin() -> dict:
-    result_dict = {}
+    results_dict = {}
 
     for _ in range(10000):
         coins_flips_list = [random.choice((0, 1)) for _ in range(10)]
         heads_count = coins_flips_list.count(0)
 
-        if heads_count in result_dict:
-            result_dict[heads_count] += 1
+        if heads_count in results_dict:
+            results_dict[heads_count] += 1
 
         else:
-            result_dict[heads_count] = 1
+            results_dict[heads_count] = 1
 
-    for key, value in result_dict.items():
-        result_dict[key] = (value / 10000) * 100
+    for key, value in results_dict.items():
+        results_dict[key] = (value / 10000) * 100
 
-    return result_dict
+    return results_dict
