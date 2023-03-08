7187c77f369c93b2215ed040d50bad0505a38d1d
NVMBR13 <108105455+NVMBR13@users.noreply.github.com>
2023-03-08 17:55:49 +0200

    v1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import random
+
+
+def flip_coin() -> None:
+
+    flips = {case: case for case in range(0, 11)}
+
+    for case in range(0, 10001):
+        flips[sum(random.choices([1, 0], k=10))] += 1
+
+    for i in flips:
+        flips[i] = flips[i] / 100
+
+    return flips
