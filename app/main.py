ebe752fe56804d824fd414277c330f565c7e09b3
kostya-kononenko <kkononenko3@gmail.com>
2023-03-17 18:48:00 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+from random import randint
+
+
+def flip_coin() -> dict:
+    result = {i: 0 for i in range(11)}
+    for _ in range(10000):
+        orel = sum([randint(0, 1) for _ in range(10)])
+        result[orel] += round((1 * 100 / 10000), 2)
+    return result
