64f607a7c948a4475933ee3d8282e346fdd41b05
KirillMelanich <kirillmelanich@gmail.com>
2023-03-23 14:04:31 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    results = {i: 0 for i in range(11)}
+
+    for i in range(10000):
+        heads = 0
+        for _ in range(10):
+            if random.random() < 0.5:
+                heads += 1
+        results[heads] += 1
+
+    for i in range(11):
+        results[i] /= 100
+
+    return results
