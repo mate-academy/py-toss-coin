12390193a8ac935045bd2846a0f632f644779762
Oleksandr Yanchuk <sashajanchuck13@gmail.com>
2023-04-14 10:35:47 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.stats import norm
 import random
 
 
@@ -15,19 +12,3 @@ def flip_coin() -> dict:
     for key in results:
         results[key] = round(results[key] / 100, 2)
     return results
-
-
-def draw_gaussian_distribution_graph(results: dict) -> None:
-    x_coord = np.arange(0, 11, 1)
-    y_coord = [results[i] for i in x_coord]
-    mean = sum(x_coord * y_coord) / 100
-    variance = \
-        sum([(xi - mean) ** 2 * yi for xi, yi in zip(x_coord, y_coord)])\
-        / 100
-    std_dev = variance ** 0.5
-    plt.bar(x_coord, y_coord)
-    plt.plot(x_coord, norm.pdf(x_coord, mean, std_dev) * 100, "r")
-    plt.xlabel("Number of heads")
-    plt.ylabel("Percentage of cases")
-    plt.title("Gaussian distribution of coin flips")
-    plt.show()
