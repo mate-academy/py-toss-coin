f4ee618bac54ca7f00432aaa371bc8f94db0ea56
Mykola <nickkozlov90@gmail.com>
2023-04-18 01:03:08 +0300

    flip coin done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    times_dict = {a: 0 for a in range(0, 11)}
+    for i in range(10000):
+        lap = sum([random.randint(0, 1) for j in range(10)])
+        times_dict.update({lap: times_dict[lap] + 1})
+    result = {
+        key: round(value / 10000 * 100, 2)
+        for key, value in times_dict.items()
+    }
+    return result
