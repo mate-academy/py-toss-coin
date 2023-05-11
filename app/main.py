c5343d328b4f88157730453177c0f48d704a3e96
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-05-11 20:00:50 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    coin = ["head", "tails"]
+    heads_dropped = {}
+    for times in range(11):
+        head_times_appear = 0
+        for flip in range(10000):
+            count_heads = sum(1 for _ in range(10)
+                              if random.choice(coin) == "head")
+            if count_heads == times:
+                head_times_appear += 1
+        percentage = 100 * (head_times_appear / 10000)
+        heads_dropped[times] = percentage
+    return heads_dropped
