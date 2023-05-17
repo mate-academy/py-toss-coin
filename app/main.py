02ddf2f840c0d8b30fdfa485edbfe3a354f14990
Volodymyr <elohimwz@gmail.com>
2023-05-17 10:44:28 +0300

    'Solution#tdt'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,42 @@
-# write your code here
+from matplotlib import pyplot as plt
+from random import randint
+
+
+def flip_coin() -> dict:
+    dict_of_probabilities = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+        9: 0,
+        10: 0
+    }
+    rang = 10000
+    for _ in range(rang):
+        count_head = 0
+        for _ in range(10):
+            head = randint(0, 1)
+            if head == 1:
+                count_head += 1
+        dict_of_probabilities[count_head] += 1 / rang
+
+    return dict_of_probabilities
+
+
+def draw_gaussian_distribution_graph() -> None:
+    special_dict = flip_coin()
+    x_points = [number for number in special_dict]
+    y_points = [round(number * 100) for number in special_dict.values()]
+
+    plt.plot(x_points, y_points)
+    plt.show()
+
+
+if __name__ == "__main__":
+    print(flip_coin())
+    draw_gaussian_distribution_graph()
