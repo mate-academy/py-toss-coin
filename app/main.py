42bf875c8ebeb4582ed3522eec006dabe368140b
Volodymyr Bohomol <8773927@gmail.com>
2023-05-29 10:20:06 +0300

    had writen `draw_gaussian_distribution_graph` function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    results = {k: v for k, v in enumerate([0] * 11)}
+    for i in range(10000):
+        heads = 0
+        for _ in range(10):
+            if random.choice(["heads", "tails"]) == "heads":
+                heads += 1
+        results[heads] += 1
+    res_percent = {k: v / 100 for (k, v) in results.items()}
+    return res_percent
