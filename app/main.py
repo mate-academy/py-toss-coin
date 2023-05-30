4245c5081f2d22c19db8b233aec8987fb578f2e8
Vladyslav Kaminskyi <119516716+VladyslavKaminskyi@users.noreply.github.com>
2023-05-30 21:52:29 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def flip_coin() -> dict:
+    result = {i: 0 for i in range(11)}
+    for _ in range(10000):
+        heads = 0
+        for _ in range(10):
+            if random.randint(0, 1):
+                heads += 1
+        result[heads] += 1
+    for key, value in result.items():
+        result[key] = round(value / 100, 2)
+    return result
+
+
+def draw_gaussian_distribution_graph() -> None:
+    for heads, count in flip_coin():
+        xpoints = np.array([heads])
+        ypoints = np.array([count])
+
+        plt.plot(xpoints, ypoints)
+        plt.show()
