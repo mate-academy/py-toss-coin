f27166dff136717f08219f6b38102caadc6cddc6
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-06-02 23:52:41 +0300

    .

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import random
-from matplotlib import pyplot as plt
-import numpy as np
+# from matplotlib import pyplot as plt
+# import numpy as np
 
 
 def flip_coin() -> dict:
@@ -16,9 +16,9 @@ def flip_coin() -> dict:
     return {key: result[key] / 100 for key in result.keys()}
 
 
-def draw_gaussian_distribution_graph(dat_dict: dict) -> None:
-    xpoints = np.array([k for k in dat_dict.keys()])
-    ypoints = np.array([v[1] for v in dat_dict.items()])
-    print([v[1] for v in dat_dict.items()])
-    plt.plot(xpoints, ypoints)
-    plt.show()
+# def draw_gaussian_distribution_graph(dat_dict: dict) -> None:
+#     xpoints = np.array([k for k in dat_dict.keys()])
+#     ypoints = np.array([v[1] for v in dat_dict.items()])
+#     print([v[1] for v in dat_dict.items()])
+#     plt.plot(xpoints, ypoints)
+#     plt.show()
