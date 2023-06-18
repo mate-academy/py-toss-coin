82d7ba730cb969b4f14c292301a26ebb8027ac27
Denys Seliverstov <catsperr322@gmail.com>
2023-06-18 18:10:46 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    coin_sides = ["heads", "tails"]
+    head_comes_up = {i: 0 for i in range(0, 11)}
+    total_heads = []
+    for _ in range(10000):
+        heads_amount = 0
+        for _ in range(10):
+            if random.choice(coin_sides) == "heads":
+                heads_amount += 1
+        total_heads.append(heads_amount)
+
+    for key in head_comes_up:
+        head_comes_up[key] = round(
+            (total_heads.count(key) / len(total_heads) * 100), 2
+        )
+
+    return head_comes_up
