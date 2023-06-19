77caa8df2b4eeab16f9109bcabacb2709788991a
Rostyslav <rostyslav.furman@gmail.com>
2023-06-19 20:15:05 +0300

    my_commit

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+from random import random
+
+
+def flip_coin() -> dict:
+    res = {}
+    attempts = 10000
+
+    for i in range(attempts):
+        head = 0
+        for attemp in range(10):
+            if random() > 0.5:
+                head += 1
+        if head in res:
+            res[head] += 1
+        else:
+            res[head] = 1
+    return {head: round((counter / attempts) * 100, 2)
+            for head, counter in res.items()}
