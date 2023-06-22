c1adb66518aa9bd21ddfd7112724a626dd235cc3
Jorjio <Jorjio22@gmail.com>
2023-06-22 20:53:22 +0000

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import random
-from matplotlib import pyplot as plt
 
 
 def flip_coin() -> dict:
@@ -18,29 +17,3 @@ def flip_coin() -> dict:
         results[key] = (results[key] / 10000) * 100
 
     return results
-
-
-def draw_gaussian_distribution_graph(results: dict) -> plt:
-    headers = list(results.keys())
-    drop = list(results.values())
-
-    mean = sum(headers[i] * drop[i] for i in range(len(headers))) / sum(drop)
-    std_dev = (
-        sum((headers[i] - mean) ** 2 * drop[i]
-            for i in range(len(headers))) / sum(drop)
-    )
-    std_dev = std_dev ** 0.5
-
-    curve_x = range(min(headers), max(headers) + 1)
-    curve_y = [
-        100 * (1 / (std_dev * (2 * 3.14) ** 0.5))
-        * (2.71 ** -(((i - mean) / std_dev) ** 2) / 2)
-        for i in curve_x
-    ]
-
-    plt.bar(headers, drop, alpha=0.7, align="center")
-    plt.plot(curve_x, curve_y, color="r")
-
-    plt.xlabel("Headers count")
-    plt.ylabel("drop %")
-    plt.title("Gaussian distribution")
