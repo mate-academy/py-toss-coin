1a24fdbaa9cf305665ee8ae97d76e6df6ff4dd5d
Max Lapchuk <makslapchuk@gmail.com>
2023-06-23 16:20:38 +0300

    Create function 'flip_coin'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    possibility_of_head_drop = {n: 0 for n in range(11)}
+
+    for _ in range(10000):
+        number_of_head_drop = 0
+        for _ in range(10):
+            drop = random.randint(0, 1)
+            number_of_head_drop += drop
+        possibility_of_head_drop[number_of_head_drop] += 1
+
+    for times in possibility_of_head_drop:
+        possibility_of_head_drop[times] = possibility_of_head_drop[times] / 100
+
+    return possibility_of_head_drop
