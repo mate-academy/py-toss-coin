e12551a3e2dd91293b1b5296b616bab04edf9f53
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-06-26 18:44:26 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,31 @@
-# write your code here
+import random
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def flip_coin():
+    counts = {i: 0 for i in range(11)}
+    for i in range(100000):
+        heads = 0
+        for j in range(10):
+            r = random.random()
+            if r < 0.5:
+                heads += 1
+        counts[heads] += 1
+    percentages = {k: math.ceil(v / 1000) / 100 for k, v in counts.items()}
+    return percentages
+
+
+def draw_gaussian_distribution_graph():
+    data = np.random.normal(0, 1, 1000)
+
+    plt.title("Gaussian Distribution")
+    plt.xlabel("Heads count")
+    plt.ylabel("Drop percentage")
+    plt.show()
+
+
+draw_gaussian_distribution_graph()
+print(flip_coin())
+
