b1adc1d460b0eb6e40a4a8f204b6c73de6dfe6a9
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-06-29 19:46:42 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,31 +1,41 @@
-import random
+from random import randint
 import matplotlib.pyplot as plt
 
 
 def flip_coin() -> dict:
-    result = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0,
-              6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
-    for i in range(100000):
-        heads = 0
-        for coin in range(10):
-            coin = random.randint(0, 1)
-            if coin == 0:
-                heads += 1
-        result[heads] += 1
-    for key in result:
-        result[key] = round(result[key] / 100, 2)
-    return result
+    dict_of_probabilities = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+        9: 0,
+        10: 0
+    }
+    rang = 10000
+    for _ in range(rang):
+        count_head = 0
+        for _ in range(10):
+            head = randint(0, 1)
+            if head == 1:
+                count_head += 1
+        dict_of_probabilities[count_head] += 1
+
+    return {
+        number: count / rang * 100
+        for number, count in dict_of_probabilities.items()
+    }
 
 
 def draw_gaussian_distribution_graph() -> None:
-    data = flip_coin()
-    x_point = list(data.keys())
-    y_point = list(data.values())
-    plt.plot(x_point, y_point)
-    plt.title("Gaussian Distribution Graph")
-    plt.xlabel("Value")
-    plt.ylabel("Frequency")
-
+    special_dict = flip_coin()
+    x_points = [number for number in special_dict]
+    y_points = [number for number in special_dict.values()]
+    plt.plot(x_points, y_points)
     plt.show()
 
 
