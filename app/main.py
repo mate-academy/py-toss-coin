195f9f50b0a205483b92f7bc86c013bd5f11ae5b
Alek Ilchenko <yaichenko87@gmail.com>
2023-07-03 10:47:13 +0100

    Fixed flake8 errors

--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 import random
 
 
-
 def flip_coin() -> dict:
     result = {}
     count_heads = 0
     for i in range(10000):
         count_heads = 0
-        for j in range(10):
+        for _ in range(10):
             if random.randint(0, 1) == 1:
                 count_heads += 1
         if count_heads in result.keys():
