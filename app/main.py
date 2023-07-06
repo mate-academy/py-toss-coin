0a3722669ae1ae01b108f7b80bd51ac505d79c82
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-07-06 11:28:59 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,41 +1,33 @@
-from random import randint
+import random
 from matplotlib import pyplot as plt
 
 
 def flip_coin() -> dict:
-    dict_of_probabilities = {
-        0: 0,
-        1: 0,
-        2: 0,
-        3: 0,
-        4: 0,
-        5: 0,
-        6: 0,
-        7: 0,
-        8: 0,
-        9: 0,
-        10: 0
-    }
-    rang = 10000
-    for _ in range(rang):
-        count_head = 0
-        for _ in range(10):
-            head = randint(0, 1)
-            if head == 1:
-                count_head += 1
-        dict_of_probabilities[count_head] += 1
+    result = []
 
-    return {
-        number: count / rang * 100
-        for number, count in dict_of_probabilities.items()
-    }
+    for _ in range(10000):
+        toss_coin = ""
+        for i in range(10):
+            toss_coin += str(random.randint(0, 1))
+
+        result.append(toss_coin.count("0"))
+
+    flip_coin_result = {}
+
+    for i in range(11):
+        flip_coin_result[i] = round((result.count(i) / 10000) * 100, 2)
+
+    return flip_coin_result
 
 
-def draw_gaussian_distribution_graph() -> None:
-    special_dict = flip_coin()
-    x_points = [number for number in special_dict]
-    y_points = [number for number in special_dict.values()]
-    plt.plot(x_points, y_points)
+def draw_gaussian_distribution_graph():
+    data = flip_coin()
+    var_x = list(data.keys())
+    var_y = list(data.values())
+    plt.plot(var_x, var_y)
+    plt.title("Gaussian Distribution Graph")
+    plt.xlabel("Value")
+    plt.ylabel("Frequency")
     plt.show()
 
 
