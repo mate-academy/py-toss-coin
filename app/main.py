76298dfe9caf9c0ad3de473c80564733c4cd9a2d
Kateryna Semesenko <semesenkoekaterina@gmail.com>
2023-07-21 00:09:33 +0300

    Implemented

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import random
+
+
+def flip_coin(cases: int = 10000) -> dict:
+
+    result_list = [
+        sum(random.randint(0, 1) for _ in range(10))
+        for _ in range(cases)
+    ]
+
+    result_dict = {}
+
+    for num in set(result_list):
+        result_dict[num] = round(result_list.count(num) / cases * 100, 2)
+
+    return result_dict
