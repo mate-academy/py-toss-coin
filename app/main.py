6a66c9cb4fa1b3f14fa59f608de8584b2302ccc7
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-08-03 00:10:33 +0300

    solution try 2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from random import choice
-import matplotlib.pyplot as plt
 
 
 def flip_coin() -> dict:
@@ -20,23 +19,3 @@ def flip_coin() -> dict:
         index: round(((counts[index] / trials) * 100), 2)
         for index in keys
     }
-
-
-def draw_gaussian_distribution_graph(data: dict) -> None:
-    percents = [num for num in range(0, 101, 10)]
-    plt.style.use("seaborn-v0_8")
-
-    fig, ax = plt.subplots()
-    ax.plot(percents, data.values(), linewidth=3)
-
-    ax.set_title("Gaussian distribution", fontsize=24)
-    ax.set_ylabel("Drop percentage %", fontsize=14)
-    ax.set_xlabel("Heads count", fontsize=14)
-
-    ax.tick_params(axis="both", labelsize=14)
-
-    plt.show()
-
-
-if __name__ == "__main__":
-    draw_gaussian_distribution_graph(flip_coin())
