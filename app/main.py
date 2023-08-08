13d6814f3c205d49a21aeaf8241d6e52231296b7
Artem Putsenko <artempucenko3@gmail.com>
2023-08-08 20:10:30 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+from random import choice
+
+
+def flip_coin() -> dict:
+    possibilities = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0,
+                     6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
+    for _ in range(10000):
+        count = 0
+        for _ in range(10):
+
+            flip = choice([0, 1])
+            if flip == 1:
+                count += 1
+
+        possibilities[count] += 1
+
+    for i in range(11):
+        possibilities[i] = round((possibilities[i] / 10000) * 100, 2)
+    return possibilities
