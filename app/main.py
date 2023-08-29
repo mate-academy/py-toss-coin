d68bd366b5ccf70a95a11bcfd0de36d380d3fbc6
Oleh Bohatov <obohatov@gmail.com>
2023-08-29 12:27:50 +0200

    Fixed solution

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import random
 
 def flip_coin() -> dict:
     result = {i: 0 for i in range(11)}
-    for _ in range(10000):
+    for _ in range(100000):
         count = 0
         for _ in range(10):
             coin_side = random.randint(0, 1)
@@ -11,5 +11,5 @@ def flip_coin() -> dict:
                 count += 1
         result[count] += 1
     for key, value in result.items():
-        result[key] = (value / 10000) * 100
+        result[key] = (value / 100000) * 100
     return result
