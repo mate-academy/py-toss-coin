053ea189377595d7ecccf2438e837332ee7f160f
jinjo <vitaly.tyagunov@gmail.com>
2023-08-31 10:24:20 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+import random
+
+
+def flip_coin(cases: int = 9999, times: int = 11) -> dict:
+    result = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+        9: 0,
+        10: 0
+    }
+    for case in range(cases):
+        heads_times = sum(random.choice([0, 1]) for _ in range(1, times))
+        result[heads_times] = result.get(heads_times, 0) + 1
+    for heads, count in result.items():
+        result[heads] = round((result[heads] / cases * 100), 2)
+    return result
