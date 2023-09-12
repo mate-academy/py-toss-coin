451f65cca7c9ec489d85da3c4a13a2f07c3ae2c9
Daniil Hinzburh <2ilgan1601@gmail.com>
2023-09-12 22:11:34 +0300

    task completed (1)

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import random
+
+
+def flip_coin(num_flips: int = 10000, num_coins: int = 10) -> dict:
+    results = {i: 0 for i in range(num_coins + 1)}
+
+    for _ in range(num_flips):
+        num_heads = sum(random.randint(0, 1) for _ in range(num_coins))
+        results[num_heads] += 1
+
+    for key, value in results.items():
+        results[key] = (value / num_flips) * 100
+
+    return results
