4ab5e07e49524c38fabdacdaef2248e7e0ee94a0
struhanets <posseydon87@gmail.com>
2023-09-14 09:57:57 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,28 +2,39 @@ import random
 
 import matplotlib.pyplot as plt
 
+import numpy as np
+
 
 def flip_coin() -> dict:
     result = []
+
     flipping = {}
-    counter = 10000
-    while counter:
-        flipping_random = random.randint(0, 10)
-        result.append(flipping_random)
-        counter -= 1
+    for _ in range(10000):
+        head_count = 0
+        for _ in range(10):
+            if random.choice([0, 1]) == 0:
+                head_count += 1
+        result.append(head_count)
 
     for item in result:
         flipping[item] = result.count(item) / 100
-
     return dict(sorted(flipping.items()))
 
 
 def draw_gaussian_distribution_graph() -> None:
-    x_point = flip_coin().keys()
-    y_point = flip_coin().values()
+    flipping_data = flip_coin()
+    x_point = np.array(list(flipping_data.keys()))
+    y_point = np.array(list(flipping_data.values()))
 
     plt.plot(x_point, y_point)
     plt.xlabel("Heads count")
     plt.ylabel("Drop percentage %")
 
+    # Задаємо діапазони осей x і y
+    plt.xlim(0, 10)  # Шкала x від 0 до 10
+    plt.ylim(0, 100)  # Шкала y від 0 до 100
+
     plt.show()
+
+
+draw_gaussian_distribution_graph()
