dbf28e4234b661b7ad4bfbe9cfd55cf97b2d1428
Vitalii2906 <fahivchik1@gmail.com>
2023-09-21 14:24:18 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+import random
+
+
+def flip_coin(num_cases=10000):
+    results = {i: 0 for i in range(11)}
+
+    for _ in range(num_cases):
+        heads_count = 0
+        for _ in range(10):
+            coin_flip = random.randint(0, 1)
+            heads_count += coin_flip
+
+        results[heads_count] += 1
+
+    total_cases = float(num_cases)
+    percentages = {key: (value / total_cases) * 100 for key, value in results.items()}
+
+    return percentages
+
+
+# Test the function
+result = flip_coin()
+print(result)
+
