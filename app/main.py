4934adc2b42c2e71108f229b1672084913fc5594
Andriy_Shumskyy <andshumskyy@gmail.com>
2023-09-23 21:35:41 +0300

    solution4

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from collections import defaultdict
 
 
-def flip_coin():
+def flip_coin() -> dict:
     num_simulations = 10000
     num_flips = 10
     results = defaultdict(int)
@@ -11,18 +11,19 @@ def flip_coin():
     for _ in range(num_simulations):
         num_heads = sum(random.choice([0, 1]) for _ in range(num_flips))
         results[num_heads] += 1
-    probable_distribution = {k: v / num_simulations * 100 for k, v in results.items()}
+    probable_distribution = {k: v / num_simulations * 100
+                             for k, v in results.items()}
     return probable_distribution
 
 
-def draw_gaussian_distribution_graph(probable_distribution):
-    x = list(probability_distribution.keys())
-    y = list(probability_distribution.values())
+def draw_gaussian_distribution_graph(probable_distribution: dict) -> None:
+    x_value = list(probability_distribution.keys())
+    y_value = list(probability_distribution.values())
 
-    plt.bar(x, y, align='center', alpha=0.7)
-    plt.xlabel('Number of Heads')
-    plt.ylabel('Percentage')
-    plt.title('Gaussian Distribution of Coin Flips')
+    plt.bar(x_value, y_value, align="center", alpha=0.7)
+    plt.xlabel("Number of Heads")
+    plt.ylabel("Percentage")
+    plt.title("Gaussian Distribution of Coin Flips")
     plt.grid(True)
 
     plt.show()
