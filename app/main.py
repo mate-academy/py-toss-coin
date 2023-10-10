1052443b4ddcac296ecef7cfd55c42a87fba2485
Alex <moleksii@gmail.com>
2023-10-10 13:42:30 +0300

    Fixed Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import random
-import matplotlib.pyplot as plt
-import numpy as np
+from matplotlib import pyplot
+import numpy
 
 
 def flip_coin() -> dict:
@@ -24,15 +24,15 @@ def draw_gaussian_distribution_graph() -> None:
     axis_x = list(data.keys())
     axis_y = list(data.values())
 
-    plt.plot(axis_x, axis_y, marker="o", linestyle="-")
-    plt.title("Gaussian Distribution of Coin Flips")
-    plt.xlabel("Number of Heads")
-    plt.ylabel("Percentage")
-    plt.grid(True)
-    mu = np.mean(axis_x)
-    sigma = np.std(axis_x)
+    pyplot.plot(axis_x, axis_y, marker="o", linestyle="-")
+    pyplot.title("Gaussian Distribution of Coin Flips")
+    pyplot.xlabel("Number of Heads")
+    pyplot.ylabel("Percentage")
+    pyplot.grid(True)
+    mu = numpy.mean(axis_x)
+    sigma = numpy.std(axis_x)
     part_of_equation = (axis_x - mu) ** 2 / (2 * sigma ** 2)
-    curve = np.exp(-part_of_equation) / (sigma * np.sqrt(2 * np.pi))
-    plt.plot(axis_x, curve, "r--")
+    curve = numpy.exp(-part_of_equation) / (sigma * numpy.sqrt(2 * numpy.pi))
+    pyplot.plot(axis_x, curve, "r--")
 
-    plt.show()
+    pyplot.show()
