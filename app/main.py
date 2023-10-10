8cd0b5acbe324d8f5b05750079108ec2aba9cbd8
Alex <moleksii@gmail.com>
2023-10-10 13:47:19 +0300

    Fixed Solution 2.0

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
 import random
-from matplotlib import pyplot
-import numpy
 
 
 def flip_coin() -> dict:
@@ -17,22 +15,3 @@ def flip_coin() -> dict:
     for key in result_dict:
         result_dict[key] = (result_dict[key] / 10000) * 100
     return result_dict
-
-
-def draw_gaussian_distribution_graph() -> None:
-    data = flip_coin()
-    axis_x = list(data.keys())
-    axis_y = list(data.values())
-
-    pyplot.plot(axis_x, axis_y, marker="o", linestyle="-")
-    pyplot.title("Gaussian Distribution of Coin Flips")
-    pyplot.xlabel("Number of Heads")
-    pyplot.ylabel("Percentage")
-    pyplot.grid(True)
-    mu = numpy.mean(axis_x)
-    sigma = numpy.std(axis_x)
-    part_of_equation = (axis_x - mu) ** 2 / (2 * sigma ** 2)
-    curve = numpy.exp(-part_of_equation) / (sigma * numpy.sqrt(2 * numpy.pi))
-    pyplot.plot(axis_x, curve, "r--")
-
-    pyplot.show()
