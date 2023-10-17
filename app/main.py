fd69b2c2a23ac166d2807bf90b36c333023864ef
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-10-17 17:25:50 +0300

    Added solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+from random import choice
+
+
+def flip_coin() -> dict:
+    num_flips = 10000
+    heads_count = [0] * 11
+    for _ in range(num_flips):
+        num_heads = sum([choice([0, 1]) for _ in range(10)])
+        heads_count[num_heads] += 1
+    result = {}
+    for num_heads, count in enumerate(heads_count):
+        result[num_heads] = round(count / num_flips * 100, 2)
+    return result
