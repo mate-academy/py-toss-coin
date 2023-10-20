c13fecb6bcc9c09cb87bc7300e112dc0e84b3802
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-10-20 18:13:20 +0300

    sol

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
-# write your code here
+import random
+
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+
+
+def flip_coin(cases: int = 10000) -> dict:
+
+    result = {tries: 0 for tries in range(11)}
+
+    for _ in range(cases):
+        count = sum(random.choice([1, 0]) for _ in range(10))
+        result[count] += (1 / cases) * 100
+    return result
+
+#
+# def draw_gaussian_distribution_graph():
+#
+#     data = flip_coin()
+#
+#     sns.barplot(x=list(data.keys()), y=list(data.values()))
+#
+#     plt.xlabel("Tries")
+#     plt.ylabel("Probability")
+#
+#     plt.show()
