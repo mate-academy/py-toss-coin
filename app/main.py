9856a8737739d55c7459113948ad9ba164bda74f
ValeriiaShvanska <ladyvaleriya13@gmail.com>
2023-10-22 17:46:48 +0300

    Fixed flake8

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ import random
 
 
 def flip_coin() -> dict:
-    result = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
+    result = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0,
+              5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
     for _ in range(10000):
         head = 0
         for time in range(10):
@@ -11,4 +12,3 @@ def flip_coin() -> dict:
                 head += 1
         result[head] += 1
     return {key: value / 100 for key, value in result.items()}
-
