954f0e1ef0e82f8ba33fce6954e4d54e9bf52e8e
Kostia <kostia3507@gmail.com>
2023-10-23 00:27:10 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    num_simulations = 10000
+    num_coin_flips = 10
+    results = {i: 0 for i in range(11)}
+
+    for _ in range(num_simulations):
+        heads_count = sum(random.choice([0, 1]) for _ in range(num_coin_flips))
+        results[heads_count] += 1
+
+    percentages = {k: (v / num_simulations * 100) for k, v in results.items()}
+    return percentages
