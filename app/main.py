6f34670e37026f1a8749c56aa9d021c5d912eb11
sakhaline <alexandra.ivva@gmail.com>
2023-10-23 00:03:29 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import random
+from collections import defaultdict
+
+
+def flip_coin() -> dict:
+    res = defaultdict(lambda: 0)
+    
+    for _ in range(0, 10000):
+        flips = sum([random.randint(0, 1) for _ in range(10)])
+        res[flips] += (1 / 100)
+
+    return res
+
