8ca15f78202af90261543ca988dbe7fa8cc1dcac
Oleksandr Pupchenko <oleksandr.pupchenko@gmail.com>
2023-10-24 00:16:52 +0200

    'sol'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    trials, flips = 10000, 10
+    results = {}
+
+    for _ in range(trials):
+        heads = sum(random.choice([0, 1]) for _ in range(flips))
+        results[heads] = results.get(heads, 0) + 1
+
+    total_trials = trials
+    percentages = {key: (value / total_trials * 100)
+                   for key, value in results.items()}
+
+    return percentages
