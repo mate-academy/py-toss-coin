9eb2824aa6bb4e082f8616d50cbd8077f2f57b49
Vladyslav Pidborskyi <pidborskii17@gmail.com>
2023-10-23 23:20:19 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    results = {i: 0 for i in range(10 + 1)}
+    for _ in range(10000):
+        heads_count = 0
+        for _ in range(10):
+            if random.random() < 0.5:
+                heads_count += 1
+        results[heads_count] += 1
+
+    probabilities = {
+        key: (value / 10000) * 100
+        for key, value in results.items()
+    }
+
+    return probabilities
