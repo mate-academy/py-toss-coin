1e99190f4701b838088f0b69933484584da1caaa
Olexandr Hontarenko <sanya221138@gmail.com>
2023-10-30 12:10:37 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,28 @@
-# write your code here
+from random import randint
+
+
+def flip_coin() -> dict:
+    result = {
+
+    }
+
+    for i in range(11):
+        result[i] = 0
+
+    for _ in range(10000):
+        coin_eagle = 0
+        for i in range(10):
+            eagle = randint(0, 1)
+
+            if eagle:
+                coin_eagle += 1
+
+        result[coin_eagle] += 1
+
+    for i in range(0, 11):
+        result[i] = round(result[i] / 100, 2)
+
+    return result
+
+
+flip_coin()
