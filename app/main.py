a3ad4107e21a12952fc341c4a04ffea225261805
Vyacheslav Vasylenko <vvg04021968@gmail.com>
2023-10-30 09:41:16 +0200

    Solution_flip-coin

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    trials, flips = 10000, 10
+    results = {}
+
+    for _ in range(trials):
+        heads = sum(random.choice([0, 1]) for _ in range(flips))
+        if not results.get(heads):
+            results[heads] = 0
+        results[heads] += 1
+
+    percentages = {key: (value / trials * 100)
+                   for key, value in results.items()}
+
+    return percentages
