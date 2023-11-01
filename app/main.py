0be7de56ed342d881c43b03dfcf13691df0c95f5
Your_Name Your_Surname <your@email>
2023-11-01 16:19:46 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,37 @@
-# write your code here
+import random
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+def flip_coin() -> dict:
+    res_dict = {}
+    count_flip = 0
+
+    while count_flip < 10001:
+        buf_count = 0
+        for _ in range(10):
+            if random.choice([0, 1]):
+                buf_count += 1
+        res_dict_value = res_dict.get(buf_count, 0)
+        res_dict[buf_count] = res_dict_value + 1
+        count_flip += 1
+
+    for key in res_dict:
+        res_dict[key] = round(res_dict[key] / 100, 2)
+    return res_dict
+
+
+def draw_gaussian_distribution_graph() -> None:
+    date_dict = flip_coin()
+    x = np.array(list(date_dict.keys()))
+    y = np.array(list(date_dict.values()))
+
+    plt.plot(x, y)
+
+    plt.title("Gaussian distribution")
+    plt.xlabel("Heads count")
+    plt.ylabel("Drop percentage %")
+
+    plt.show()
