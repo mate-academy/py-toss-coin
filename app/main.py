cea7d1c78455d2a3a684435a009251817e139f1d
Hryhoriy <hryhoriy.bilous@gmail.com>
2023-11-13 20:46:34 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def flip_coin() -> dict:
     results = {flip: 0 for flip in range(11)}
 
     for _ in range(num_cases):
-        count_of_head = sum(random.randint(0, 1) for _ in range(10))
+        count_of_head = round(sum(random.randint(0, 1) for _ in range(10)))
         results[count_of_head] += 1
 
     res = {key: value / num_cases * 100 for key, value in results.items()}
