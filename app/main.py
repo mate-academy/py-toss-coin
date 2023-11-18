e94347e5406f8299bce9cc30cf27fd53a0dfdf7f
Aeros72 <pashamelnyk2004@gmail.com>
2023-11-18 15:21:34 +0200

    Implemented py-flip-coin

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+from random import randint
+import matplotlib.pyplot as plt
+
+
+def flip_coin() -> dict:
+    cases = 10000
+    result = {i: 0 for i in range(11)}
+
+    for _ in range(cases):
+        heads = sum(randint(0, 1) for _ in range(10))
+        result[heads] += 1
+
+    result = {key: value / cases * 100 for key, value in result.items()}
+    return result
+
+
+def draw_gaussian_distribution_graph(data: dict) -> None:
+    keys = list(data.keys())
+    values = list(data.values())
+    plt.plot(keys, values)
+    plt.title("Gaussian distribution")
+    plt.xlabel("Heads count")
+    plt.ylabel("Drop percentage %")
+    plt.show()
