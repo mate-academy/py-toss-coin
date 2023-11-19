e22ee4449ec7f93b01f07ecd15fc9f92da362a4d
Alex <oleksa.shcherbakov@gmail.com>
2023-11-19 11:53:33 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,16 +1,27 @@
 import random
-import math
 
 
-def flip_coin(times: int) -> dict:
-    result = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
-    for i in range (times):
+def flip_coin() -> dict:
+    result = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+        9: 0,
+        10: 0
+    }
+    for i in range(10000):
         quantity = 0
-        for ii in range (10):
-            heads = random.randint(0,1)
+        for ii in range(10):
+            heads = random.randint(0, 1)
             if heads == 0:
                 quantity += 1
         result[quantity] += 1
     for key, value in result.items():
-        result[key] = round(value / times * 100, 2)
+        result[key] = round(value / 10000 * 100, 2)
     return result
