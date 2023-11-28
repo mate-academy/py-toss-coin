ba3b02d05c14682b28b026b3f9a3c949014d145a
Marinel444 <380978907444m@gmail.com>
2023-11-28 15:24:12 +0200

    add anotations

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 import matplotlib.pyplot as plt
 
 
-def flip_coin():
+def flip_coin() -> dict:
     results = defaultdict(int)
     for _ in range(10000):
         heads_count = sum(random.choice([0, 1]) for _ in range(10))
@@ -15,10 +15,10 @@ def flip_coin():
     return dict(results)
 
 
-def draw_gaussian_distribution_graph(flip_results):
+def draw_gaussian_distribution_graph(flip_results: dict) -> None:
     heads, percentages = zip(*sorted(flip_results.items()))
     plt.bar(heads, percentages)
-    plt.xlabel('Number of Heads')
-    plt.ylabel('Percentage (%)')
-    plt.title('Gaussian Distribution of Coin Flips')
+    plt.xlabel("Number of Heads")
+    plt.ylabel("Percentage (%)")
+    plt.title("Gaussian Distribution of Coin Flips")
     plt.show()
