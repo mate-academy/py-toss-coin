c4fc2f420da9b71d6d48c2d326dfa9bcd29015d1
Liliya <liliyakhu@ukr.net>
2023-12-14 21:16:27 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 import random
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def flip_coin() -> dict:
@@ -11,21 +13,24 @@ def flip_coin() -> dict:
             res = random.randint(1, 2)
             if res == 2:
                 result += 1
-
         result_dict[result] += 1
     for key in result_dict:
         result_dict[key] = result_dict[key] * 100 / 10000
-    # xks = list(result_dict.keys())
-    # yks = list(result_dict.values())
-    # xpoints = np.array(xks)
-    # ypoints = np.array(yks)
-    # plt.plot(xpoints, ypoints)
-    # plt.ylim(0, 100)
-    # plt.xlim(0, 10)
-    # plt.yticks(list(range(0, 101, 10)))
-    # plt.xticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    # plt.title("Gaussian distribution")
-    # plt.xlabel("Heads count")
-    # plt.ylabel("Drop percentage %")
-    # plt.show()
     return result_dict
+
+
+def draw_gaussian_distribution_graph() -> None:
+    res_dict = flip_coin()
+    xks = list(res_dict.keys())
+    yks = list(res_dict.values())
+    xpoints = np.array(xks)
+    ypoints = np.array(yks)
+    plt.plot(xpoints, ypoints)
+    plt.ylim(0, 100)
+    plt.xlim(0, 10)
+    plt.yticks(list(range(0, 101, 10)))
+    plt.xticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    plt.title("Gaussian distribution")
+    plt.xlabel("Heads count")
+    plt.ylabel("Drop percentage %")
+    plt.show()
