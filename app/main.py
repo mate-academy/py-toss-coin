2cc16e994ab8e6dfe3c231787a4339dac6da3854
Denysov Viacheslav <wdenisov113@gmail.com>
2023-12-16 11:07:36 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,38 @@
-# write your code here
+import random
+import matplotlib.pyplot as plt
+
+
+def flip_coin() -> dict:
+    result_dict = {num: 0 for num in range(11)}
+
+    for _ in range(10000):
+        result = sum(random.randint(1, 2) == 2 for _ in range(10))
+        result_dict[result] += 1
+
+    for key in result_dict:
+        result_dict[key] = result_dict[key] * 100 / 10000
+
+    return result_dict
+
+
+def draw_gaussian_distribution_graph() -> None:
+    data = flip_coin()
+
+    # Extract keys and values from the dictionary
+    x_values = list(data.keys())
+    y_values = list(data.values())
+
+    # Create a bar chart
+    plt.bar(x_values, y_values, color="skyblue")
+
+    # Add labels and a title
+    plt.xlabel("Heads count")
+    plt.ylabel("Drop percentage")
+    plt.title("Gaussian distribution")
+
+    # Show the plot
+    plt.show(block=True)
+
+
+flip_coin()
+draw_gaussian_distribution_graph()
