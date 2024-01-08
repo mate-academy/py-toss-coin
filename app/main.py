8feb5d98fac3bf4cb986f374ec8d7c2d41210b45
Zoresław Dmytryszyn <zoreslav.dmytryshyn@gmail.com>
2024-01-08 17:38:19 +0100

    resolve an issue with matplotlib

--- app/main.py
+++ app/main.py
@@ -6,47 +6,40 @@ import random
 
 def flip_coin() -> dict:
 
-    result = {}
+    result = {num: 0 for num in range(0, 12)}
 
-    for case in range(15000):
+    for case in range(10000):
         nr_of_heads_out_of_10 = 0
         for num in range(1, 11):
             heads_or_tails = random.randint(0, heads := 1)
             if heads_or_tails == heads:
                 nr_of_heads_out_of_10 += 1
 
-        if nr_of_heads_out_of_10 in result:
-            result[nr_of_heads_out_of_10] += 1
-        else:
-            result[nr_of_heads_out_of_10] = 1
+        result[nr_of_heads_out_of_10] += 1
 
-    for key in result.keys():
-        result[key] /= (15000 / 100)
+    return {key: value / 100 for key, value in result.items()}
 
-    return dict(sorted(result.items()))
 
-# # EXTRA TASK
-# def draw_gaussian_distribution_graph(func: Callable) -> None:
-#     result_dict = func()
-#     x_points = []
-#     y_points = []
-#
-#     for key, values in result_dict.items():
-#         x_points.append(key)
-#         y_points.append(values)
-#
-#     pyplot.plot(
-#         x_points,
-#         y_points,
-#         "h-.m",
-#         ms=10,
-#         mec="k",
-#         mfc="#00CED1"
-#     )
-#
-#     pyplot.title("Gaussian distribution")
-#     pyplot.xlabel("Heads count")
-#     pyplot.ylabel("Drop percentage %")
-#     pyplot.xlim(0, 10)
-#     pyplot.ylim(0, 100)
-#     pyplot.show()
+"""
+EXTRA TASK
+def draw_gaussian_distribution_graph(func: Callable) -> None:
+    result_dict = func()
+    x_points = [key for key in result_dict.keys()]
+    y_points = [value for value in result_dict.values()]
+
+    pyplot.plot(
+        x_points,
+        y_points,
+        "h-.m",
+        ms=10,
+        mec="k",
+        mfc="#00CED1"
+    )
+
+    pyplot.title("Gaussian distribution")
+    pyplot.xlabel("Heads count")
+    pyplot.ylabel("Drop percentage %")
+    pyplot.xlim(0, 10)
+    pyplot.ylim(0, 100)
+    pyplot.show()
+"""
