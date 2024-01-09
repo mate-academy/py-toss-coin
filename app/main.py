67973038cdc1170614167874f5c4b0251e91cb4b
Borys Kirichenko <boryskirichenko99@gmail.com>
2024-01-09 18:03:17 +0100

    flake8 fixed

--- app/main.py
+++ app/main.py
@@ -2,10 +2,9 @@ import random
 from collections import defaultdict
 
 
-def flip_coin():
+def flip_coin() -> dict:
     counts = defaultdict(int)
     for _ in range(10000):
-        flips = [random.choice(['Heads', 'Tails']) for _ in range(10)]
-        counts[flips.count('Heads')] += 1
-    return {k: v/100 for k, v in sorted(counts.items())}
-
+        flips = [random.choice(["Heads", "Tails"]) for _ in range(10)]
+        counts[flips.count("Heads")] += 1
+    return {k: v / 100 for k, v in sorted(counts.items())}
