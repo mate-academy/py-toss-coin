18069e8a349e9d0e6301956a7b2e58e9625395a6
Borys Kirichenko <boryskirichenko99@gmail.com>
2024-01-09 17:44:15 +0100

    all tests passed

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+import random
+from collections import defaultdict
+
+
+def flip_coin():
+    counts = defaultdict(int)
+    for _ in range(10000):
+        flips = [random.choice(['Heads', 'Tails']) for _ in range(10)]
+        counts[flips.count('Heads')] += 1
+    return {k: v/100 for k, v in sorted(counts.items())}
+
