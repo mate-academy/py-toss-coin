448e2e70fe0ad5c49a67148de7a9727f0495aab7
Oleksii Mileiko <alexexile1987@gmail.com>
2024-01-18 01:26:38 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    head_roll = {}
+    for _ in range(10000):
+        heads = sum(random.choices([0, 1], k=10))
+        if heads in head_roll:
+            head_roll[heads] += 1
+        else:
+            head_roll[heads] = 1
+    result = {key: (value / 100) for key, value in head_roll.items()}
+    return result
