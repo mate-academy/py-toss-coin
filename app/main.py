e350477283e15f201bb879219189e864c4f08b31
xrkxdd <nazar.mc28@gmail.com>
2024-01-18 05:26:33 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    result_of_heads = {}
+    for _ in range(10000):
+        heads = sum(random.choice([0, 1]) for _ in range(10))
+        result_of_heads[heads] = result_of_heads.get(heads, 0) + 1
+
+    result = {key: (value / 100) for key, value in result_of_heads.items()}
+    return result
