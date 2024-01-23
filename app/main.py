ef821ead1791c20a978e13b1b4907376819531e2
Vladyslav Pliashechnyk <vlad.pliashechnyk@gmail.com>
2024-01-23 16:30:29 +0200

    py-flip-coin

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+from random import randint
+
+
+def flip_coin() -> dict:
+    cases = sorted(
+        [sum(1 for _ in range(10) if randint(0, 1) == 1) for _ in range(10000)]
+    )
+    return {key: (cases.count(key) / 10000) * 100 for key in set(cases)}
