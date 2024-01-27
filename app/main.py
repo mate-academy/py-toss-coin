53b4baa29bd477c8eb432c549ddd50d3ac4e350c
Veronika <veronikaboeva935@gmail.com>
2024-01-27 12:19:58 +0100

    Solution 27.01.2024

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    coin = ["heads", "tails"]
+
+    result = {}
+
+    for _ in range(10000):
+        number_of_heads = 0
+
+        for _ in range(10):
+            if random.choice(coin) == "heads":
+                number_of_heads += 1
+
+        if number_of_heads not in result:
+            result[number_of_heads] = 1 / 10000 * 100
+        result[number_of_heads] += 1 / 10000 * 100
+
+    return result
