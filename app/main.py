4fbd0bbf8b64e75cf3f8233c3aaaad1c429876d7
Maksym Nimych <maksimnimitch@gmail.com>
2024-01-27 17:18:22 +0200

    Comment draw & matplotlib

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import random
-from matplotlib import pyplot
-import numpy
+# from matplotlib import pyplot
+# import numpy
 
 
 def flip_coin() -> dict:
@@ -18,21 +18,21 @@ def flip_coin() -> dict:
     for count, drops in enumerate(drops_count):
         result[count] = (drops * 100) / 10000
 
-    x_points = [key for key in result.keys()]
-    y_points = [value for value in result.values()]
-    draw_gaussian_distribution_graph(x_points, y_points)
+    # x_points = [key for key in result.keys()]
+    # y_points = [value for value in result.values()]
+    # draw_gaussian_distribution_graph(x_points, y_points)
 
     return result
 
 
-def draw_gaussian_distribution_graph(x_points: dict, y_points: dict) -> None:
-    x_plot_points = numpy.array(x_points)
-    y_plot_points = numpy.array(y_points)
+# def draw_gaussian_distribution_graph(x_points: dict, y_points: dict) -> None:
+#     x_plot_points = numpy.array(x_points)
+#     y_plot_points = numpy.array(y_points)
 
-    pyplot.plot(x_plot_points, y_plot_points)
+#     pyplot.plot(x_plot_points, y_plot_points)
 
-    pyplot.title("Gaussian distribution")
-    pyplot.xlabel("Heads count")
-    pyplot.ylabel("Drop percentage %")
+#     pyplot.title("Gaussian distribution")
+#     pyplot.xlabel("Heads count")
+#     pyplot.ylabel("Drop percentage %")
 
-    pyplot.show()
+#     pyplot.show()
