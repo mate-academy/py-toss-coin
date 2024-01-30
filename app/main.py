d3a6ee1f5aea80592b45eda36a762af1d82dba31
PythonZem <pythonzem@gmail.com>
2024-01-31 00:04:58 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,18 +1,34 @@
 import random
+import numpy
+import matplotlib.pyplot as plt
 
 
 def flip_coin() -> dict:
-    result = {i: 0 for i in range(11)}
+    result = {i: 0 for i in range(10)}
 
     for _ in range(10000):
         heads_count = sum(random.choice([0, 1]) for _ in range(0, 10))
-        result[heads_count] += heads_count
+        result[heads_count] = result.get(heads_count, 0) + 1
 
-    total = sum(value for value in result.values())
     for key, value in result.items():
-        result[key] = (value / total) * 100
+        result[key] = round((value / 10000) * 100, 2)
 
     return result
 
-print(flip_coin())
 
+def draw_gaussian_distribution_graph():
+    data = flip_coin()
+    x = numpy.array(list(data.keys()))
+    y = numpy.array(list(data.values()))
+    print(x)
+    print(y)
+    print(data)
+
+    plt.plot(x, y)
+
+    plt.ylabel("Drop percentage %")
+    plt.xlabel("Heads count")
+
+    plt.show()
+
+draw_gaussian_distribution_graph()
