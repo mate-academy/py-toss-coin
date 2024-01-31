5f5a18f9b90398ab140739ada677362cdaea8b9d
Nick Illich <nickillich96@gmail.com>
2024-01-31 16:51:51 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from matplotlib import pyplot
+# from matplotlib import pyplot
 
 import random
 
@@ -16,16 +16,16 @@ def flip_coin(num_trials: int = 10000, num_flips: int = 10) -> dict:
     return result
 
 
-def draw_gaussian_distribution_graph() -> None:
-
-    x_heads = list(flip_coin().keys())
-    y_percentages = list(flip_coin().values())
-
-    pyplot.plot(x_heads, y_percentages)
-    pyplot.xlabel("Heads count")
-    pyplot.ylabel("Drop percentage %")
-
-    pyplot.show()
-
-
-draw_gaussian_distribution_graph()
+# def draw_gaussian_distribution_graph() -> None:
+#
+#     x_heads = list(flip_coin().keys())
+#     y_percentages = list(flip_coin().values())
+#
+#     pyplot.plot(x_heads, y_percentages)
+#     pyplot.xlabel("Heads count")
+#     pyplot.ylabel("Drop percentage %")
+#
+#     pyplot.show()
+#
+#
+# draw_gaussian_distribution_graph()
