d51885684b4889e7d45b4f9a42c5220a876fc4e4
Kyrylo Chepenko <chip22835ya@gmail.com>
2024-01-31 15:22:02 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import random
+from collections import Counter
+
+
+def flip_coin(num_flips: int = 10, num_trials: int = 10000) -> int:
+    results_counter = Counter()
+
+    for _ in range(num_trials):
+        num_heads = sum(random.choice([0, 1]) for _ in range(num_flips))
+        results_counter[num_heads] += 1
+
+    results_percentage = {
+        key: (value / num_trials) * 100
+        for key, value in results_counter.items()
+    }
+
+    return results_percentage
