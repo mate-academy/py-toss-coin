8232885e736511cb81df17378e3de266abaf13f9
Serhii Buryk <sergmarten21@gmail.com>
2024-01-31 22:45:13 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+from random import choice
+
+
+def flip_coin() -> dict:
+    variant = ["head", "coin"]
+    possible_count_times = {i: 0 for i in range(0, 11)}
+
+    for _ in range(10000):
+        count = 0
+        for i in range(10):
+            if choice(variant) == "head":
+                count += 1
+        possible_count_times[count] += 1
+    result = {i: round(possible_count_times[i] / 100, 2)
+              for i in possible_count_times.keys()}
+    return result
