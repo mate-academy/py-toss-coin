91615d57fd819e1b60c6d2797bf22fb922db04a9
Serhii Haiduchyk <jaegarn141@gmail.com>
2024-02-01 09:16:13 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+from random import randint
+
+
+def flip_coin() -> dict:
+    coin_flips = {}
+
+    for _ in range(10000):
+        heads_count = sum(randint(0, 1) for _ in range(10))
+        coin_flips[heads_count] = coin_flips.get(heads_count, 0) + 1
+
+    percents = {key: (value / 10000) * 100 for key,
+                value in coin_flips.items()}
+
+    return percents
