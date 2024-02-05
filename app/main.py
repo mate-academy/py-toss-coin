072b4e3d2cea24fc655c16f1b36b7784952f261c
angelzagniy <angelzagniy@gmail.com>
2024-02-05 20:35:40 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    possible_heads = {
+        0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0
+    }
+    variants = ["head", "tail"]
+    for _ in range(10000):
+        count = 0
+        for _ in range(10):
+            variant = random.choice(variants)
+            if variant == "head":
+                count += 1
+        possible_heads[count] += 0.01
+
+    return possible_heads
