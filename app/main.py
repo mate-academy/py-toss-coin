31c8422e556f0e6ec944ec9d7fcda35b5126698d
sergey <38cerg12021979@jmal.com>
2024-02-12 14:30:47 +0200

    12_02_24

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import random
+import matplotlib.pyplot as plt
+
+
+def flip_coin() -> dict:
+    results = {}
+
+    for _ in range(10000):
+        num_heads = sum(random.choice([0, 1]) for _ in range(10))
+        results[num_heads] = results.get(num_heads, 0) + 1
+
+    for num_heads in results:
+        number = results[num_heads]
+        results[num_heads] = round((number / float(10000)) * 100, 3)
+    return results
+
+
+def draw_gaussian_distribution_graph(funck: dict) -> None:
+    xpoints = ([key for key in funck.keys()])
+    ypoints = ([vel for vel in funck.values()])
+    plt.bar(xpoints, ypoints)
+    plt.show()
