4340f8ad073df8cf89b472d527f001267555e3c0
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-02-17 16:20:40 +0200

    Fixed flake8

--- app/main.py
+++ app/main.py
@@ -3,7 +3,10 @@ from collections import Counter
 
 
 def flip_coin(num_trials: int = 10_000, num_flips: int = 10) -> dict:
-    results = [sum(random.choice([0, 1]) for _ in range(num_flips)) for _ in range(num_trials)]
+    results = [
+        sum(random.choice([0, 1]) for _ in range(num_flips))
+        for _ in range(num_trials)
+    ]
     counts = Counter(results)
     percentages = {k: (v / num_trials) * 100 for k, v in counts.items()}
 
