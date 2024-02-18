762f132b309c4d066875dc8ae34132563a661435
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-02-18 23:22:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    heads_list = [sum(random.choices([1, 0], k=10)) for _ in range(10_000)]
+
+    heads_occurrence = {}
+    for number in range(11):
+        heads_occurrence[number] = heads_list.count(number) / 100
+
+    return heads_occurrence
