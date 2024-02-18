7b9bb6761698acf2cdf1a5198340b41156559f55
Molchanov Ihor <=>
2024-02-19 01:19:56 +0200

    solved task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    heads = {}
+    for _ in range(10000):
+        count = 0
+        for i in range(10):
+            if random.choice([True, False]):
+                count += 1
+        if count not in heads:
+            heads[count] = 0
+        heads[count] += 1
+    result = {key: round(value / 100, 2) for key, value in heads.items()}
+    return result
