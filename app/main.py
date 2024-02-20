927e769c9628dcd6a472edce3e1fad0a7eb43be2
RomanHlodann <roman.hlodann@gmail.com>
2024-02-20 10:54:44 +0200

    implemented func to create and draw distribution based on fliping coin

--- app/main.py
+++ app/main.py
@@ -1,1 +1,27 @@
-# write your code here
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def flip_coin() -> dict:
+    coin = ["heads", "tails"]
+    result = {}
+    for _ in range(10000):
+        counter = 0
+        for _ in range(10):
+            if random.choice(coin) == "heads":
+                counter += 1
+        result[counter] = result.get(counter, 0) + 1
+    return {
+        key: value / 100 for key, value in result.items()
+    }
+
+
+def draw_gaussian_distribution_graph(coin_distribution: dict) -> None:
+    distribution = dict(sorted(coin_distribution.items()))
+    xpoints = np.array(list(distribution.keys()))
+    ypoints = np.array(list(distribution.values()))
+
+    plt.plot(xpoints, ypoints)
+    plt.show()
