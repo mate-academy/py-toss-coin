ba4a2c2c7b8a7e3345b8778fec39da1cc5ea3460
Alexander <sasacihun@gmail.com>
2024-02-26 13:05:48 +0200

    Added plot

--- app/main.py
+++ app/main.py
@@ -1,12 +1,26 @@
 import random
+import matplotlib.pyplot as plt
 
 
 def flip_coin() -> dict:
     dict_store = {}
-    for _ in range(10000):
+    repeat = 10000
+    for _ in range(repeat):
         heads = [random.randint(0, 1) for _ in range(10)]
         how_many_heads = len([h for h in heads if h == 1])
-        dict_store[how_many_heads] = dict_store.get(how_many_heads, 0) - 1
+        tmp = dict_store.get(how_many_heads, 0)
+        dict_store[how_many_heads] = dict_store.get(how_many_heads, 0) + 1
 
-    res = {k: (v / -10000) * 100 for k, v in dict_store.items()}
+    res = {k: (v / repeat) * 100 for k, v in dict_store.items()}
     return res
+
+
+def draw_gaussian_distribution_graph(res) -> None:
+    x = res.keys()
+    y = res.values()
+    plt.plot(x, y)
+    plt.show()
+
+
+res = flip_coin()
+draw_gaussian_distribution_graph(res)
