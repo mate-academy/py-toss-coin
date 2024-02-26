d0d9c4db5d574c249e313a8b39c9b6ae9954a9dd
Alexander <sasacihun@gmail.com>
2024-02-26 10:18:51 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    dict_store = {}
+    for _ in range(10000):
+        heads = [random.randint(0, 1) for _ in range(10)]
+        how_many_heads = len([h for h in heads if h == 1])
+        dict_store[how_many_heads] = dict_store.get(how_many_heads, 0) - 1
+
+    res = {k: (v / -10000) * 100 for k, v in dict_store.items()}
+    return res
