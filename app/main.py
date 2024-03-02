be26895d1c83a9b200f801b82143963e091e5978
TarasFirst <firsttaras@mail.com>
2024-03-02 10:43:15 +0200

    Solution_GTA_3

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import random
 from collections import defaultdict
-import matplotlib.pyplot
 
 
 def flip_coin() -> dict:
@@ -11,16 +10,3 @@ def flip_coin() -> dict:
     for key in results:
         results[key] = round((results[key] / 10000) * 100, 2)
     return dict(sorted(results.items()))
-
-
-def draw_gaussian_distribution_graph(data: dict) -> None:
-    keys = list(data.keys())
-    values = list(data.values())
-
-    matplotlib.pyplot.figure(figsize=(10, 5))
-    matplotlib.pyplot.bar(keys, values, color="skyblue")
-    matplotlib.pyplot.xlabel("Number of Heads")
-    matplotlib.pyplot.ylabel("Percentage")
-    matplotlib.pyplot.title("Gaussian Distribution of Coin Flips")
-    matplotlib.pyplot.xticks(keys)
-    matplotlib.pyplot.show()
