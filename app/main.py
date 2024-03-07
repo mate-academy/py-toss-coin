da741ee908ea9fb48f195a418f5b5fe2ad162812
Maksim <sma4no.7@gmail.com>
2024-03-07 12:48:01 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import random
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+
+def flip_coin() -> dict:
+    dict_result = {i: 0 for i in range(11)}
+    for _ in range(10000):
+        heads_count = sum(random.choice([0, 1]) for _ in range(10))
+        dict_result[heads_count] += 1
+    persentage = {
+        key: value / 10000 * 100
+        for key, value in dict_result.items()
+    }
+    return persentage
+    # xpoints = np.array(list(persentage.keys()))
+    # ypoints = np.array(list(persentage.values()))
+    # plt.plot(xpoints, ypoints)
+    # plt.show()
