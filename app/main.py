4d47ce872335f1178a914fc363de52d8e9665f23
Lumideus <d.bilan2004@gmail.com>
2024-03-21 09:25:45 +0200

    add gaussian_distribution

--- app/main.py
+++ app/main.py
@@ -11,12 +11,3 @@ def flip_coin() -> dict:
              for j in range(10000)].count(number) / 100
         for number in range(11)
     }
-
-
-def draw_gaussian_distribution_graph(distribution_data: dict):
-    xpoints = np.array(list(distribution_data.keys()))
-    ypoints = np.array(list(distribution_data.values()))
-    plt.plot(xpoints, ypoints)
-    plt.xlabel("Count of head")
-    plt.ylabel("Probability")
-    plt.show()
