f862b5df47fd8290d0628a4999e8c5ada757551e
Viesich <v.s.vesich@gmail.com>
2024-03-26 15:37:26 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -16,20 +16,3 @@ def flip_coin() -> dict:
             results[heads_count] = 1
     percentages = {key: value / 100 for key, value in results.items()}
     return percentages
-
-
-def draw_gaussian_distribution_graph() -> None:
-    results = flip_coin()
-    x_coord = list(results.keys())
-    y_coord = list(results.values())
-
-    plt.plot(x_coord, y_coord, marker="o", linestyle="-")
-    plt.xlabel()
-    plt.ylabel()
-    plt.title()
-    x_smooth = np.linspace(min(x_coord), max(x_coord), 100)
-    y_smooth = np.interp(x_smooth, x_coord, y_coord)
-    plt.plot(x_smooth, y_smooth)
-
-    plt.grid(True)
-    plt.show()
