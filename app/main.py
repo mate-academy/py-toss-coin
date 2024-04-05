5624b46197e1a42728d363ec38cc7a49ed55577b
Oleksandr Matviichuk <oleksandr.matviychuk@gmail.com>
2024-04-05 16:52:19 +0300

    Solution1

--- app/main.py
+++ app/main.py
@@ -1,20 +1,5 @@
 import random
 import matplotlib.pyplot as plt
-import numpy as np
-
-
-def draw_gaussian_distribution_graph(data: dict) -> None:
-    x_points = np.array([num for num in range(11)])
-    y_points = np.array([data[num] for num in range(11)])
-    plt.plot(x_points, y_points)
-    plt.title("Gaussian distribution")
-    plt.xlabel("Heads count")
-    plt.ylabel("Drop percentage %")
-    plt.ylim(0, 100)
-    plt.xlim(0, 10)
-    plt.yticks(range(0, 101, 10))
-    plt.xticks(range(0, 11, 1))
-    plt.show()
 
 
 def flip_coin() -> dict:
@@ -30,6 +15,20 @@ def flip_coin() -> dict:
     for num in range(11):
         result_dict[num] = round(result_dict[num] / 100, 2)
     return result_dict
+
+
+def draw_gaussian_distribution_graph(data: dict) -> None:
+    x_points = [num for num in range(11)]
+    y_points = [data[num] for num in range(11)]
+    plt.plot(x_points, y_points)
+    plt.title("Gaussian distribution")
+    plt.xlabel("Heads count")
+    plt.ylabel("Drop percentage %")
+    plt.ylim(0, 100)
+    plt.xlim(0, 10)
+    plt.yticks(range(0, 101, 10))
+    plt.xticks(range(0, 11, 1))
+    plt.show()
 
 
 if __name__ == "__main__":
