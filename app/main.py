130544aaa81a216a01418b4ed4dd2e9a68b658a6
Alona Sorochynska <alona.sorochynska@gmail.com>
2024-04-12 11:23:56 +0100

    a

--- app/main.py
+++ app/main.py
@@ -2,17 +2,15 @@ import random
 import matplotlib.pyplot as plt
 
 
-def flip_coin() -> dict:
-    num_trials = 10000
+def flip_coin(num_trials: int = 10000) -> dict:
     result = {key: 0 for key in range(11)}
-
     for _ in range(num_trials):
         heads_count = sum(1 for _ in range(10)
                           if random.choice(["H", "T"]) == "H")
         result[heads_count] += 1
-
     probabilities = {key: (count / num_trials) * 100
                      for key, count in result.items()}
+
     return probabilities
 
 
@@ -25,8 +23,3 @@ def create_plot(probabilities: dict) -> None:
     plt.xlabel("Heads, times")
     plt.ylabel("Probability, %")
     plt.show()
-
-
-flip_result = flip_coin()
-print(flip_result)
-create_plot(flip_result)
