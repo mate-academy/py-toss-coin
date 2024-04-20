f8e39d3336b8ba03c296414d4ef764aaee9ff0fd
Oleksandr Klymyk <klymyksasha@gmail.com>
2024-04-20 04:39:45 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict[int, float]:
+    flip_data = [random.choices((0, 1), k=10).count(1) for _ in range(10_000)]
+    statistic = {
+        num: round((0.01 * flip_data.count(num)), 2) for num in range(11)
+    }
+    return statistic
