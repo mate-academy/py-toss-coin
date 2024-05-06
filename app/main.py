7cbe37cfe31c19306f0fc9d3a153b060a7503ed5
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2024-05-06 15:15:09 +0300

    sol

--- app/main.py
+++ app/main.py
@@ -1,27 +1,28 @@
 import random
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def flip_coin() -> dict:
-    head_times = {
-        0: 0,
-        1: 0,
-        2: 0,
-        3: 0,
-        4: 0,
-        5: 0,
-        6: 0,
-        7: 0,
-        8: 0,
-        9: 0,
-        10: 0,
-    }
+    head_times = {i: 0 for i in range(11)}
     for case in range(10_000):
-        heads = 0
-        for coin in range(10):
-            if random.choice(("head", "tail")) == "head":
-                heads += 1
-
-        head_times[heads] += 1
+        head_times[sum(
+            1 if random.choice(("head", "tail")) == "head" else 0
+            for head in range(10)
+        )] += 1
 
     return {head_time: round(head_times[head_time] / 100, 2)
             for head_time in head_times}
+
+
+def draw_gaussian_distribution_graph(data: dict) -> None:
+    x_points = np.array(data.keys())
+    y_points = np.array(data.values())
+
+    plt.xlim(0, 11)
+    plt.ylim(0, 100)
+    plt.title("Gaussian distribution")
+    plt.xlabel("Heads count")
+    plt.ylabel("Drop percentage %")
+    plt.plot(x_points, y_points)
+    plt.show()
