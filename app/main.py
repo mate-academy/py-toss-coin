4702a0658ae38d0e287c110f284fd934b130eea8
daikj <tyutyunniksolomia@gmail.com>
2024-05-24 16:49:01 +0300

    implement flip-coin

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+from random import randint
+
+
+def flip_coin() -> dict:
+    result = {}
+
+    for _ in range(10000):
+        heads = sum(randint(0, 1) for _ in range(10))
+        result[heads] = result.get(heads, 0) + 1
+
+        flips = sum(result.values())
+    for head, count in result.items():
+        result[head] = round(count / flips * 100, 2)
+    return result
