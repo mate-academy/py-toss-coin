8062dc88272f830aaf1227a165a712fcd245d198
Mariia Movchan <mariia.sergeevna.k@gmail.com>
2024-05-25 08:01:55 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+from random import randint
+
+
+def flip_coin() -> dict:
+    flip_results = []
+    results_of_flipping_heads = {}
+    for _ in range(10000):
+        flip_coin_10_times = [randint(0, 1) for _ in range(10)]
+        flip_results.append(flip_coin_10_times.count(1))
+    for i in range(11):
+        results_of_flipping_heads[i] = round(flip_results.count(i) / 100, 2)
+    return results_of_flipping_heads
