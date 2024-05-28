79fcd725da4529f95599ff1d62e349d0ac916db0
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-05-28 19:08:16 +0300

    comment graph

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from random import randint
-import matplotlib.pyplot as plt
-import numpy as np
+# import matplotlib.pyplot as plt
+# import numpy as np
 
 
 def flip_coin() -> dict:
@@ -14,13 +14,13 @@ def flip_coin() -> dict:
     return results_of_flipping_heads
 
 
-def draw_gaussian_distribution_graph() -> None:
-    result_of_flipping = flip_coin()
-    x_point = np.array([key for key in result_of_flipping.keys()])
-    y_point = np.array([value for value in result_of_flipping.values()])
-
-    plt.plot(x_point, y_point)
-    plt.title("Gaussian distribution")
-    plt.xlabel("Heads count")
-    plt.ylabel("Drop percentage %")
-    plt.show()
+# def draw_gaussian_distribution_graph() -> None:
+#     result_of_flipping = flip_coin()
+#     x_point = np.array([key for key in result_of_flipping.keys()])
+#     y_point = np.array([value for value in result_of_flipping.values()])
+#
+#     plt.plot(x_point, y_point)
+#     plt.title("Gaussian distribution")
+#     plt.xlabel("Heads count")
+#     plt.ylabel("Drop percentage %")
+#     plt.show()
