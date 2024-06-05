908f4af24531aff85832754b38ac9603b213a104
Viktor Shcherbak <dev.psychologist@gmail.com>
2024-06-05 23:11:06 +0300

    Gaussian distribution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    result = {}
+    for i in range(10000):
+        heads = sum(random.randint(0, 1) for j in range(10))
+        result[heads] = result.get(heads, 0) + 1
+
+    for key in result:
+        result[key] = (result[key] / 10000) * 100
+
+    return result
