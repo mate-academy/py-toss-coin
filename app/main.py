882d9289228bb4bce22ca0ac7af3a1ab16e49316
necobra <necobrane105@gmail.com>
2024-06-06 16:39:29 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    amount_of_simulation = 10000
+
+    drop_rate = {}
+    for i in range(11):
+        drop_rate[i] = 0
+
+    for _ in range(amount_of_simulation):
+        heads_count = 0
+        for _ in range(10):
+            if random.choice([True, False]):
+                heads_count += 1
+        drop_rate[heads_count] += 1
+
+    return {
+        key: round(value / amount_of_simulation * 100, 3)
+        for key, value in drop_rate.items()
+    }
