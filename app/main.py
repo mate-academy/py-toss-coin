ea961cf7dc7eb455510a29a219442f97a340864f
unlie9 <unlie.devops9@gmail.com>
2024-06-10 17:43:02 +0300

    sol1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+from random import randint
+
+
+def flip_coin() -> dict:
+    result = {}
+    for i in range(0, 11):
+        result[i] = 0
+
+    for _ in range(10000):
+        count = 0
+        for _ in range(10):
+            if randint(0, 1):
+                count += 1
+        result[count] += 1
+
+    attempts = 10000
+    for key in result:
+        result[key] = (result[key] / attempts) * 100
+
+    return result
