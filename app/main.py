ab21d17d9d0fc0f3dafdae10b3c62afab63cad61
Nazar <legoman1858@gmail.com>
2024-06-11 02:40:01 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    heads_count = {i: 0 for i in range(11)}
+    num_trials = 10000
+    for _ in range(num_trials):
+        flips = [random.choice([1, 0]) for _ in range(10)]
+        num_heads = sum(1 for flip in flips if flip == 1)
+        heads_count[num_heads] += 1
+    heads_percent = {k: v / num_trials * 100 for k, v in heads_count.items()}
+    heads_percent_rounded = {k: round(v, 2) for k, v in heads_percent.items()}
+    return heads_percent_rounded
