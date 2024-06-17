167d62d6a0065fd5c214df57419fd5b14ecaabd7
Maksym Protsak <maksym.protsak@gmail.com>
2024-06-17 13:25:08 +0300

    Solution 17-06-2024 13-25

--- app/main.py
+++ app/main.py
@@ -11,30 +11,4 @@ def flip_coin() -> dict:
                 head += 1
         result[head] += 0.01
 
-    # coin_sides = ["Heads", "Tails"]
-    # tosses_of_ten_times = {}
-    # for i in range(10000):
-    #     times_in_tosses = {}
-    #     for time_of_toss in range(10):
-    #         toss = random.choice(coin_sides)
-    #         times_in_tosses[time_of_toss] = toss
-    #     tosses_of_ten_times[i] = times_in_tosses
-    # statistics_of_ten_times = {i: 0 for i in range(10)}
-    # heads_in_zero_time = 0
-    # for toss in tosses_of_ten_times.values():
-    #     if toss[0] == "Heads":
-    #         heads_in_zero_time += 1
-    #
-    # for key in statistics_of_ten_times:
-    #     heads_in_zero_time = 0
-    #     for toss in tosses_of_ten_times.values():
-    #         if toss[key] == "Heads":
-    #             heads_in_zero_time += 1
-    #     statistics_of_ten_times[key] = round(
-    #         100 / len(tosses_of_ten_times) * heads_in_zero_time, 2
-    #     )
-    # return statistics_of_ten_times
     return result
-
-if __name__ == "__main__":
-    print(flip_coin())
