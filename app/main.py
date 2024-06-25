ac7ef419f29c9ed748f0f2ede289b814aeead5a6
K. Petrychenko <catpetrychenko@gmail.com>
2024-06-25 23:33:33 +0300

    Solution 

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 import random
 from collections import defaultdict
 import matplotlib.pyplot as plt
-from typing import Dict
 
 
-def flip_coin() -> Dict[int, float]:
+def flip_coin() -> dict:
     trials = 10000
     results = defaultdict(int)
 
@@ -13,7 +12,7 @@ def flip_coin() -> Dict[int, float]:
         results[heads] += 1
 
     for key in results:
-        results[key] = (results[key] / trials) * 100
+        results[key] = (results[key] / trials) * 100.0
 
     return dict(results)
 
@@ -22,9 +21,12 @@ def draw_gaussian_distribution_graph() -> None:
     results = flip_coin()
 
     sorted_results = dict(sorted(results.items()))
+    xpoints = list(sorted_results.keys())
+    ypoints = list(sorted_results.values())
 
-    plt.bar(sorted_results.keys(), sorted_results.values(), color="blue")
+    plt.plot(xpoints, ypoints, marker="o", linestyle="-")
     plt.xlabel("Number of Heads")
     plt.ylabel("Percentage")
     plt.title("Distribution of Heads in 10 Coin Flips (10,000 Trials)")
+    plt.grid(True)  # Add grid lines
     plt.show()
