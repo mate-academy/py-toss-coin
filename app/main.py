517c50a323e56426c42e8f8b740c17a70cd0a9c3
Andrew Chugreev <andrey.4.1985@gmail.com>
2024-06-29 00:43:06 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -3,21 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# def flip_coin(num_simulations: int = 10000, num_flips: int = 10) -> dict:
-#     results = {key: 0 for key in range(num_flips + 1)}
-#     for _ in range(num_simulations):
-#         flips = [random.choice(["H", "T"]) for _ in range(num_flips)]
-#         num_heads = flips.count("H")
-#         results[num_heads] += 1
-#     print(results)
-#     for key in results:
-#         results[key] = (results[key] / num_simulations) * 100
-#     return results
-#
-#
-# data = flip_coin()
-# print(data)
-
 def flip_coin() -> dict:
     result = {}
     all_heads = 0
