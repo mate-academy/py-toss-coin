ad01ad3b3cc826b55770e834ce885249720c872f
Lebid Dmytro <lebiddmytriy@gmail.com>
2024-07-01 16:36:02 +0300

    Solution+

--- app/main.py
+++ app/main.py
@@ -9,10 +9,7 @@ def flip_coin(trials: int = 10000) -> dict[int, float]:
         heads_count = sum(random.choice([0, 1]) for _ in range(10))
         results[heads_count] += 1
 
-    for key in results:
-        results[key] = (results[key] / trials) * 100
-
-    return results
+    return {key: (value / trials) * 100 for key, value in results.items()}
 
 
 def draw_gaussian_distribution_graph(results: dict[int, float]) -> None:
