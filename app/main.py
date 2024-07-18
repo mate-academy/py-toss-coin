bc19056876f4dd778ad9c1f2e677c9f0206da0fb
maksym_korniev <maksymkorniev88@gmail.com>
2024-07-18 12:59:41 +0300

    created a flip coin method

--- app/main.py
+++ app/main.py
@@ -1,1 +1,42 @@
-# write your code here
+import random
+import matplotlib.pyplot as plt
+
+
+def imitate_coins_flip(cases: int) -> dict:
+    result_dict = {}
+    for _ in range(cases):
+        head_drops = 0
+        for i in range(10):
+            is_heads = random.randint(0, 1)  # 1 - head
+            if is_heads == 1:
+                head_drops += 1
+
+        if head_drops in result_dict:
+            result_dict[head_drops] += 1
+        else:
+            result_dict[head_drops] = 1
+
+    return {key: val for key, val in
+            sorted(result_dict.items(), key=lambda ele: ele[0])}
+
+
+def flip_coin() -> dict:
+    cases = 10000
+    result_dict = imitate_coins_flip(cases)
+
+    for key in result_dict:
+        result_dict[key] = round((result_dict[key] * 100) / cases, 2)
+    return result_dict
+
+
+print(flip_coin())
+
+
+def draw_gaussian_distribution_graph() -> None:
+    cases = 10000
+    result_dict = imitate_coins_flip(cases)
+    plt.plot(result_dict.keys(), result_dict.values())
+    plt.show()
+
+
+# draw_gaussian_distribution_graph()
