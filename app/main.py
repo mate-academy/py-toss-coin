8823175ae9588950c0dc56e562cf3d5c3951665b
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-08-09 15:45:38 +0200

    implement py-flip-coin

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+import random
+from collections import Counter
+
+
+def flip_coin() -> dict:
+    result = []
+    for _ in range(10000):
+        count = sum([random.choice([0, 1]) for _ in range(10)])
+        result.append(count)
+    count_heads = Counter(result)
+    dict_result = {k: (v / 10000) * 100 for k, v in count_heads.items()}
+    return dict(sorted(dict_result.items()))
