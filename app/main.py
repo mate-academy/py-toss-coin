c6d996f455e6558e3748ba2c9b0022af1f87e619
Halyna Rohatska <rohatskahr@gmail.com>
2024-08-10 17:13:57 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -10,19 +10,3 @@ def flip_coin() -> dict:
         heads_count[dropped.count(1)] += 1
 
     return {k: round(v / 10000 * 100, 2) for k, v in heads_count.items()}
-
-
-result = flip_coin()
-
-
-def draw_gaussian_distribution_graph(points: dict) -> None:
-    point_x = [k for k in points.keys()]
-    point_y = [v for v in points.values()]
-
-    plt.plot(point_x, point_y)
-    plt.xlabel("Heads count")
-    plt.ylabel("Drop percentage %")
-    plt.show()
-
-
-draw_gaussian_distribution_graph(result)
