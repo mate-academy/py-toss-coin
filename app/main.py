5bd499954d4d80d4dc70b9ce19cbea9f378df339
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-08-12 17:27:11 +0300

    solving3

--- app/main.py
+++ app/main.py
@@ -1,23 +1,15 @@
 import random
-from typing import Dict
 
 
-def flip_coin() -> Dict[int, float]:
-    result_dict: Dict[int, int] = {i: 0 for i in range(11)}
+def flip_coin() -> dict:
+    dic = dict.fromkeys(range(11), 0)
+    total_trials = 10000
 
-    for _ in range(10000):
-        times = 0
-        for _ in range(10):
-            if random.randint(0, 1) == 1:
-                times += 1
+    for _ in range(total_trials):
+        heads_count = sum(random.randint(1, 2) == 1 for _ in range(10))
+        dic[heads_count] += 1
 
-        result_dict[times] += 1
+    for key in dic:
+        dic[key] = round((dic[key] / total_trials) * 100, 2)
 
-    probability_dict: Dict[int, float]\
-        = {key: round(value / 10000, 3)
-           for key, value in result_dict.items()}
-
-    return probability_dict
-
-
-print(flip_coin())
+    return dic
