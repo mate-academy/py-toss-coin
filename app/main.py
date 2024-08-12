a1fe025cbf3a21d108cde31f5bc57bea6df947a0
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-08-12 17:14:31 +0300

    solving

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    result_dict = {i: 0 for i in range(11)}
+
+    for _ in range(10000):
+        times = 0
+        for _ in range(10):
+            if random.randint(0, 1) == 1:
+                times += 1
+
+        result_dict[times] += 1
+
+    for key in result_dict:
+        result_dict[key] = round(result_dict[key] / 10000, 3)
+
+    return result_dict
+
+
+print(flip_coin())
