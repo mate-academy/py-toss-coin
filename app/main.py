45c1b18d9cddddcf7a47fdb9e950fc8d0518e710
Lev <lev2099@gmail.com>
2024-10-07 10:17:21 +0300

    no use numpy

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
-from numpy import random
+import random
 
 
 def flip_coin(num_experiments: int = 10000) -> dict:
     statistics = {i: 0 for i in range(11)}
     for _ in range(num_experiments):
-        result = random.binomial(n=10, p=0.5)
+        result = sum(random.choice([0, 1]) for _ in range(10))
         statistics[result] += 1
+        print(statistics)
 
     percentages = {key: (value / num_experiments) * 100
                    for key, value in statistics.items()}
