f63aaef2c0cbb2641fe5a666528a12a93650ce9f
Lev <lev2099@gmail.com>
2024-10-07 10:00:03 +0300

    flake

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 from numpy import random
 
 
-def flip_coin(num_experiments=10000) -> dict:
-    statistics = {i: 0 for i in range(11)}  # Створюємо словник для підрахунку випадків від 0 до 10 орлів
-
+def flip_coin(num_experiments: int = 10000) -> dict:
+    statistics = {i: 0 for i in range(11)}
     for _ in range(num_experiments):
-        result = random.binomial(n=10, p=0.5)  # Підкидання 10 монет за один експеримент
-        statistics[result] += 1  # Збільшуємо кількість для отриманого результату
+        result = random.binomial(n=10, p=0.5)
+        statistics[result] += 1
 
     # Обчислюємо відсотки для кожної кількості орлів
-    percentages = {key: (value / num_experiments) * 100 for key, value in statistics.items()}
+    percentages = {key: (value / num_experiments) * 100
+                   for key, value in statistics.items()}
 
     return percentages
 
