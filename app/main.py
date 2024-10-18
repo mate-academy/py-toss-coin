1560f3e3fc8bc23cec80ca055e3dcb64943d7b0c
yura <pollo7964132@gmail.com>
2024-10-18 22:59:48 +0300

    fixed flake8 errors

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 import random
 import matplotlib.pyplot as plt
 
+
 def flip_coin() -> dict:
     scrim = ["head", "back"]
     result = {num: 0 for num in range(11)}
 
     for _ in range(10000):
-        head_count = sum(1 for _ in range(10) if random.choice(scrim) == "head")
+        head_count = sum(
+            1 for _ in range(10) if random.choice(scrim) == "head"
+        )
         result[head_count] += 1
 
     for key in result.keys():
@@ -15,16 +18,22 @@ def flip_coin() -> dict:
     return result
 
 
-def draw_gaussian_distribution_graph(data: dict):
-    x = list(data.keys())
-    y = list(data.values())
+def draw_gaussian_distribution_graph(data: dict) -> None:
+    x_lable = list(data.keys())
+    y_lable = list(data.values())
 
     plt.figure(figsize=(8, 6))
-    plt.plot(x, y, marker='o', linestyle='-', color='b', label='Distribution')
+    plt.plot(
+        x_lable, y_lable,
+        marker="o",
+        linestyle="-",
+        color="b",
+        label="Distribution"
+    )
 
-    plt.title('Distribution of Heads in 10 Coin Flips (10000 Experiments)')
-    plt.xlabel('Number of Heads')
-    plt.ylabel('Percentage')
+    plt.title("Distribution of Heads in 10 Coin Flips (10000 Experiments)")
+    plt.xlabel("Number of Heads")
+    plt.ylabel("Percentage")
 
     plt.grid(True)
 
