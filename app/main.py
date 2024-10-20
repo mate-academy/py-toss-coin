1c1c9800d89cce38e9527976ee2b14719d664dcb
Andrey Dementyev <dementyev.andreyy@gmail.com>
2024-10-20 15:37:27 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    flips = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
+
+    for i in range(1, 10001):
+        count = 0
+        for i in range(10):
+            choice = random.randint(0, 1)
+            if choice:
+                count += 1
+        flips[count] += 1
+    for key, value in flips.items():
+        flips[key] = round((value / 10000) * 100, 2)
+    return flips
