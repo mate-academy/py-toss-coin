2a1b74e65974889f071f88f52cd61a36719e1a21
Serhii <serhijkrasa64uk5@gmail.com>
2024-10-21 21:12:58 +0300

    solution 2

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ import random
 
 
 def flip_coin() -> dict:
-    flip_dict = {}
     result_list = []
     for _ in range(10000):
         a, b = 0, 0
@@ -26,11 +25,13 @@ def flip_coin() -> dict:
     zero_ten_counter = result_list.count(zero_ten) / len(result_list) * 100
     one_nine_counter = result_list.count(one_nine) / len(result_list) * 100
     two_eight_counter = result_list.count(two_eight) / len(result_list) * 100
-    three_seven_counter = result_list.count(three_seven) / len(result_list) * 100
+    three_seven_counter = (result_list.count(three_seven) / len(result_list)
+                           * 100)
     four_six_counter = result_list.count(four_six) / len(result_list) * 100
     five_five_counter = result_list.count(five_five) / len(result_list) * 100
     six_four_counter = result_list.count(six_four) / len(result_list) * 100
-    seven_three_counter = result_list.count(seven_three) / len(result_list) * 100
+    seven_three_counter = (result_list.count(seven_three) / len(result_list)
+                           * 100)
     eight_two_counter = result_list.count(eight_two) / len(result_list) * 100
     nine_one_counter = result_list.count(nine_one) / len(result_list) * 100
     ten_zero_counter = result_list.count(ten_zero) / len(result_list) * 100
