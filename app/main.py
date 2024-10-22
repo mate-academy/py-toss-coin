e8f7794c12bcbce33442685a56b2fbe37a933c83
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2024-10-22 11:55:36 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+from random import randint
+
+
+def flip_coin() -> dict[int: float]:
+    flip_times = dict.fromkeys(range(0, 11), 0)
+
+    for _ in range(10000):
+        temp = 0
+        for _ in range(10):
+            if randint(0, 1):
+                temp += 1
+        flip_times[temp] += 1
+
+    return {
+        key: value / 100
+        for key, value in flip_times.items()
+    }
