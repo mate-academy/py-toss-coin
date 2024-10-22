5a568b659f406de99388e2109be7730bfb98e0dc
Matvii Safonov <matveysafonov2@gmail.com>
2024-10-22 14:19:04 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import random
-import matplotlib.pyplot as plt
 
 
 def flip_coin(num_flips: int=10, num_trials: int=10000) -> float: # noqa
@@ -13,21 +12,3 @@ def flip_coin(num_flips: int=10, num_trials: int=10000) -> float: # noqa
         results[key] = round((results[key] / num_trials) * 100, 2)
 
     return results
-
-
-def draw_gaussian_distribution_graph(data: list) -> list:
-    x_coords = list(data.keys())
-    y_coords = list(data.values())
-
-    plt.bar(x_coords, y_coords, color="skyblue")
-    plt.title("Distribution of Heads in Coin Flips")
-    plt.xlabel("Number of Heads")
-    plt.ylabel("Percentage (%)")
-    plt.xticks(x_coords)
-    plt.grid(axis="y", linestyle="--", alpha=0.7)
-    plt.show()
-
-
-results = flip_coin()
-print(results)
-draw_gaussian_distribution_graph(results)
