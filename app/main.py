77afa409f6d00acf5ff32d6cf67bf0ac91482d76
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2024-10-22 12:18:56 +0300

    Add "draw_gaussian_distribution_graph" function

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from random import randint
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 def flip_coin() -> dict[int: float]:
     flip_times = dict.fromkeys(range(0, 11), 0)
@@ -15,3 +18,21 @@ def flip_coin() -> dict[int: float]:
         key: value / 100
         for key, value in flip_times.items()
     }
+
+
+def draw_gaussian_distribution_graph() -> None:
+    flip_results = flip_coin()
+
+    x_points = np.array(list(flip_results.keys()))
+    y_points = np.array(list(flip_results.values()))
+
+    plt.plot(x_points, y_points)
+    plt.title("Gaussian distribution")
+    plt.xlabel("Heads count")
+    plt.ylabel("Drop percentage %")
+
+    plt.xticks(x_points)
+    plt.ylim(0, 100)
+    plt.yticks(np.arange(0, 101, 10))
+
+    plt.show()
