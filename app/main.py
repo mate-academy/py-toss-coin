57a616b232bd7272d7a881e0d7c79aadff2f8a5f
Oleksandr Merlenko <s.merlenko@gmail.com>
2024-10-23 19:57:02 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import random
-import numpy as np
-from matplotlib import pyplot
+# import numpy as np
+# from matplotlib import pyplot
 
 
 def flip_coin(iterations: int = 10000, flips: int = 10) -> dict:
@@ -19,22 +19,22 @@ def flip_coin(iterations: int = 10000, flips: int = 10) -> dict:
     return statistics
 
 
-def draw_gaussian_distribution_graph(statistics: dict) -> None:
-    x_list = []
-    y_list = []
-    for i in range(len(statistics)):
-        x_list.append(i)
-        y_list.append(statistics[i])
-
-    x_axis = np.array(x_list)
-    y_axis = np.array(y_list)
-
-    pyplot.plot(x_axis, y_axis)
-    pyplot.ylim(0, 100)
-    pyplot.xlim(0, 10)
-
-    pyplot.title("Gaussian distribution")
-    pyplot.xlabel("Heads count")
-    pyplot.ylabel("Drop Percentage %")
-
-    pyplot.show()
+# def draw_gaussian_distribution_graph(statistics: dict) -> None:
+#     x_list = []
+#     y_list = []
+#     for i in range(len(statistics)):
+#         x_list.append(i)
+#         y_list.append(statistics[i])
+#
+#     x_axis = np.array(x_list)
+#     y_axis = np.array(y_list)
+#
+#     pyplot.plot(x_axis, y_axis)
+#     pyplot.ylim(0, 100)
+#     pyplot.xlim(0, 10)
+#
+#     pyplot.title("Gaussian distribution")
+#     pyplot.xlabel("Heads count")
+#     pyplot.ylabel("Drop Percentage %")
+#
+#     pyplot.show()
