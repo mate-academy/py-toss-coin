454bce07915a34b2520d7b5a2f581cd223d3150b
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2024-10-24 12:55:13 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    result = {i: 0 for i in range(11)}
+
+    trials = 10000
+    for _ in range(trials):
+        count = 0
+        for _ in range(10):
+            flip = random.randint(0, 1)
+            count += flip
+
+        result[count] += 1
+
+    percentage = {key: (value / trials) * 100 for key, value in result.items()}
+
+    return percentage
