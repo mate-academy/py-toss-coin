7c36a02f8f319a44607650fd4a2019cfd0949d32
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2024-11-12 13:32:53 +0200

    Solution 2.0

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ def flip_coin() -> dict[int, float]:
     heads_count = {}
 
     for _ in range(10_000):
-        count = 0
-        for _ in range(11):
-            if round(random.choice((0, 1))) == 1:
-                count += 1
+        count = sum(
+            1 for _ in range(10)
+            if round(random.random()) == 1
+        )
 
         if heads_count.get(count) is None:
             heads_count[count] = 1
@@ -18,5 +18,3 @@ def flip_coin() -> dict[int, float]:
     return {
         heads: (count / 10000) * 100 for heads, count in heads_count.items()
     }
-
-print(flip_coin())
