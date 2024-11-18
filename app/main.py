92848996ad745c117f3ac8c1161584cf905a96ac
Alya <alya.v.kravchenko@gmail.com>
2024-11-18 12:42:47 +0200

    Just flip

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import random
-import matplotlib.pyplot as plt
 
 
 def flip_coin(num_of_cases: int = 10000) -> dict:
@@ -11,23 +10,3 @@ def flip_coin(num_of_cases: int = 10000) -> dict:
 
     percentages = {k: round((v / num_of_cases) * 100, 2) for k, v in results.items()}
     return percentages
-
-print(flip_coin())
-
-
-def draw_gaussian_distribution_graph(data: dict):
-    x = list(data.keys())
-    y = list(data.values())
-
-    plt.figure(figsize=(10, 6))
-    plt.bar(x, y, color='skyblue', edgecolor='black')
-    plt.plot(x, y, color='red', marker='o', linestyle='dashed', label='Gaussian Approximation')
-
-    plt.xlabel('Heads count')
-    plt.ylabel(' Drop percentage %')
-    plt.title('Gaussian distribution')
-    plt.xticks(range(0, 11))
-    plt.grid(axis='y', linestyle='--', alpha=0.7)
-    plt.legend()
-
-    plt.show()
