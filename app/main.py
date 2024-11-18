34bb68de1d68b939339a909bd767fc3d93a6b56f
Alya <alya.v.kravchenko@gmail.com>
2024-11-18 12:47:05 +0200

    new

--- app/main.py
+++ app/main.py
@@ -1,18 +1,13 @@
 import random
 
 
-def flip_coin() -> dict:
-    result = {i: 0 for i in range(11)}
+def flip_coin(num_of_cases: int = 10000) -> dict:
+    results = {i: 0 for i in range(11)}
 
-    trials = 10000
-    for _ in range(trials):
-        count = 0
-        for _ in range(10):
-            flip = random.randint(0, 1)
-            count += flip
+    for _ in range(num_of_cases):
+        heads_count = sum(random.choice([0, 1]) for _ in range(10))
+        results[heads_count] += 1
 
-        result[count] += 1
-
-    percentage = {key: (value / trials) * 100 for key, value in result.items()}
-
-    return percentage
+    percentages = {k: round((v / num_of_cases) * 100, 2)
+                   for k, v in results.items()}
+    return percentages
