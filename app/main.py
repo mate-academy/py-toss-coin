ad35dcdd64ede8a139c23c8953d0e0e6d9cc43f2
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-11-28 10:45:11 +0200

    Added graphic

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 import random
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def flip_coin() -> dict:
@@ -24,3 +26,23 @@ def flip_coin() -> dict:
         value = result_dict[key]
         result_dict[key] = round((value / 100), 2)
     return result_dict
+
+
+if __name__ == '__main__':
+    result = flip_coin()
+    x_asics = [num for num in range(0, 11)]
+    print(x_asics)
+    y_asics = [num for num in range(0, 110, 10)]
+    occurrences = [num for num in result.values()]
+    fig, ax = plt.subplots()
+
+    ax.plot(x_asics, occurrences, "-o", label='Occurrences')
+    ax.set_ylim(0, 100)
+    ax.set_yticks([num for num in range(0, 101, 10)])
+
+
+    plt.plot(x_asics, occurrences)
+    plt.xlabel("Frequency of Occurrences")
+    plt.ylabel("Count of Occurrences")
+    plt.title(f"Flip Coin")
+    plt.show()
