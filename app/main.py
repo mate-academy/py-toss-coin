dd95d1eb5e82c7919986921258e2309c87770cc3
Serhiy <sergeynechaenko@ukr.net>
2024-12-04 00:44:41 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -6,17 +6,17 @@ def flip_coin() -> dict:
     numbers = [[random.randint(0, 1) for _ in range(10)]
                for _ in range(1, 10001)]
     sum_numbers = [sum(number) for number in numbers]
-    dict_numbers = {"0": sum_numbers.count(0) / 10000 * 100,
-                    "1": sum_numbers.count(1) / 10000 * 100,
-                    "2": sum_numbers.count(2) / 10000 * 100,
-                    "3": sum_numbers.count(3) / 10000 * 100,
-                    "4": sum_numbers.count(4) / 10000 * 100,
-                    "5": sum_numbers.count(5) / 10000 * 100,
-                    "6": sum_numbers.count(6) / 10000 * 100,
-                    "7": sum_numbers.count(7) / 10000 * 100,
-                    "8": sum_numbers.count(8) / 10000 * 100,
-                    "9": sum_numbers.count(9) / 10000 * 100,
-                    "10": sum_numbers.count(10) / 10000 * 100}
+    dict_numbers = {0: sum_numbers.count(0) / 10000 * 100,
+                    1: sum_numbers.count(1) / 10000 * 100,
+                    2: sum_numbers.count(2) / 10000 * 100,
+                    3: sum_numbers.count(3) / 10000 * 100,
+                    4: sum_numbers.count(4) / 10000 * 100,
+                    5: sum_numbers.count(5) / 10000 * 100,
+                    6: sum_numbers.count(6) / 10000 * 100,
+                    7: sum_numbers.count(7) / 10000 * 100,
+                    8: sum_numbers.count(8) / 10000 * 100,
+                    9: sum_numbers.count(9) / 10000 * 100,
+                    10: sum_numbers.count(10) / 10000 * 100}
     return dict_numbers
 
 
