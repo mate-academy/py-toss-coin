68f5634a5fa494a314c23983e167a0d2ef3e5d06
Olha Shumska <olgashumska65@gmail.com>
2024-12-05 20:59:47 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import random
+
+
+def flip_coin() -> dict:
+    coin = ["head", "bottom"]
+    cases = {i: 0 for i in range(11)}
+    for _ in range(10000):
+        count_head = 0
+        for __ in range(10):
+            side_of_coin = random.choice(coin)
+            if side_of_coin == "head":
+                count_head += 1
+        cases[count_head] += 1
+    cases_percentage = {}
+    for case in cases:
+        cases_percentage[case] = cases[case] / 100
+    return cases_percentage
