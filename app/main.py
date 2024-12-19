ed644e68e2d6e376e1b34771edc2bdc316c975d3
Andrii Ovod <ovodenok@mail.com>
2024-12-19 20:42:31 +0200

    Attempt #5 fixed incorrect variable name

--- app/main.py
+++ app/main.py
@@ -5,24 +5,27 @@ from typing import Dict
 
 def flip_coin() -> Dict[int, float]:
     num_simulations: int = 10000
-    results: Dict[int, float] = {i: 0.0 for i in range(11)}
+    simulation_results: Dict[int, float] = {i: 0.0 for i in range(11)}
     for _ in range(num_simulations):
         heads_count = sum(random.randint(0, 1) for _ in range(10))
-        results[heads_count] += 1
+        simulation_results[heads_count] += 1
 
-    for key in results:
-        results[key] = (results[key] / num_simulations) * 100
+    for key in simulation_results:
+        simulation_results[key] = (simulation_results[key] / num_simulations) * 100
 
-    return results
+    return simulation_results
 
 
-results = flip_coin()
+def draw_gaussian_distribution_graph(simulation_results: Dict[int, float]) -> None:
+    number_of_heads = list(simulation_results.keys())
+    percentage_values = list(simulation_results.values())
 
-number_of_heads = list(results.keys())
-percentage_values = list(results.values())
+    plt.bar(number_of_heads, percentage_values)
+    plt.title("Distribution of Heads in 10 Coin Flips")
+    plt.xlabel("Number of Heads")
+    plt.ylabel("Percentage")
+    plt.show()
 
-plt.bar(number_of_heads, percentage_values)
-plt.title("Distribution of Heads in 10 Coin Flips")
-plt.xlabel("Number of Heads")
-plt.ylabel("Percentage")
-plt.show()
+
+simulation_results = flip_coin()
+draw_gaussian_distribution_graph(simulation_results)
