e2f44cc0e36465fa3c8f78115c1290d540369d6c
Andrii Ovod <ovodenok@mail.com>
2024-12-19 19:17:00 +0200

    Attempt #2

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from typing import Dict
 
 def flip_coin() -> Dict[int, float]:
     num_simulations = 10000
-    results = {i: 0 for i in range(11)}
+    results: Dict[int, float] = {i: 0 for i in range(11)}
     for _ in range(num_simulations):
         heads_count = sum(random.randint(0, 1) for _ in range(10))
         results[heads_count] += 1
