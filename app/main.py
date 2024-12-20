e21f01485880dcd77523dd2871cf327f740ed4af
Nazar Slavych <slavuch232355@gmail.com>
2024-12-20 02:50:52 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import random
-import matplotlib.pyplot as plt
 
 
 def flip_coin() -> dict:
@@ -13,13 +12,3 @@ def flip_coin() -> dict:
     percentages = {k: round((v / trials) * 100, 3) for k, v in results.items()}
 
     return percentages
-
-
-def draw_gaussian_distribution_graph() -> None:
-    percentages = flip_coin()
-
-    x_cord = list(percentages.keys())
-    y_cord = list(percentages.values())
-
-    plt.plot(x_cord, y_cord)
-    plt.show()
