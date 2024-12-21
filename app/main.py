3ada27e32be2656db69abe83401ac3b607fcf3ec
Grigoriy Sventsitskiy <g.gesiod@gmail.com>
2024-12-21 23:59:13 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+import random
+from pprint import pprint
+
+
+def flip_coin() -> dict[int: float]:
+    heads_all_counts = []
+    for _ in range(10000):
+        heads_count = 0
+        for _ in range(10):
+            if random.choice(['heads', 'tails']) == 'heads':
+                heads_count += 1
+        heads_all_counts.append(heads_count)
+    return {k: heads_all_counts.count(k) / len(heads_all_counts) for k in range(11)}
+
+
+
+
+if __name__ == '__main__':
+    cache = set()
+
+    for _ in range(20):
+        coins = flip_coin()
+        pprint(coins)
+        cache.add(tuple(result for result in coins.values()))
+    ## pprint(cache)
