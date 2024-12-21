14460957228fddb46bb66c343d0aaae5a8eb9631
Grigoriy Sventsitskiy <g.gesiod@gmail.com>
2024-12-22 01:14:59 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,18 +1,36 @@
 import random
 from pprint import pprint
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def flip_coin() -> dict[int: float]:
     heads_all_counts = []
     for _ in range(10000):
         heads_count = 0
+        random.seed(random.randint(1,9999999))
         for _ in range(10):
             if random.choice(['heads', 'tails']) == 'heads':
                 heads_count += 1
         heads_all_counts.append(heads_count)
     return {k: heads_all_counts.count(k) / len(heads_all_counts) for k in range(11)}
 
+def draw_gaussian_distribution_graph():
+    coins = flip_coin()
+    xpoints = np.array(list(coins.keys()))
+    ypoints = np.array([round(x * 100, 2) for x in list(coins.values())])
 
+
+    plt.xlabel('Heads count')
+    plt.ylabel('Drop percentage %')
+    plt.title('Gaussian distribution')
+    plt.ylim([0,100])
+    plt.xlim([0,10])
+
+    plt.plot(xpoints, ypoints)
+    plt.xticks(range(11))
+    plt.yticks(range(0,101,10))
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -20,6 +38,7 @@ if __name__ == '__main__':
 
     for _ in range(20):
         coins = flip_coin()
-        pprint(coins)
+
         cache.add(tuple(result for result in coins.values()))
-    ## pprint(cache)
+    print(cache)
+    draw_gaussian_distribution_graph()
