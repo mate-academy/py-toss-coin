098d45e590742624a77e06d822f0c7e28c5a96e5
Grigoriy Sventsitskiy <g.gesiod@gmail.com>
2024-12-22 01:49:08 +0200

    Solution (pytest and flake9 - pass)

--- app/main.py
+++ app/main.py
@@ -1,39 +1,36 @@
 import random
-from pprint import pprint
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 def flip_coin() -> dict[int: float]:
-    heads_all_counts = []
+    heads_all_counts = {k: 0 for k in range(0, 11)}
     for _ in range(10000):
-        heads_count = 0
-        random.seed(random.randint(1,9999999))
-        for _ in range(10):
-            if random.choice(['heads', 'tails']) == 'heads':
-                heads_count += 1
-        heads_all_counts.append(heads_count)
-    return {k: heads_all_counts.count(k) / len(heads_all_counts) for k in range(11)}
+        heads_count = [random.randint(0, 1) for _ in range(0, 10)].count(1)
+        heads_all_counts[heads_count] += 1
+    for key, value in heads_all_counts.items():
+        heads_all_counts[key] = value / 100
+    return heads_all_counts
 
-def draw_gaussian_distribution_graph():
+
+def draw_gaussian_distribution_graph() -> None:
     coins = flip_coin()
     xpoints = np.array(list(coins.keys()))
-    ypoints = np.array([round(x * 100, 2) for x in list(coins.values())])
+    ypoints = np.array([round(x, 2) for x in list(coins.values())])
 
-
-    plt.xlabel('Heads count')
-    plt.ylabel('Drop percentage %')
-    plt.title('Gaussian distribution')
-    plt.ylim([0,100])
-    plt.xlim([0,10])
+    plt.xlabel("Heads count")
+    plt.ylabel("Drop percentage %")
+    plt.title("Gaussian distribution")
+    plt.ylim([0, 100])
+    plt.xlim([0, 10])
 
     plt.plot(xpoints, ypoints)
     plt.xticks(range(11))
-    plt.yticks(range(0,101,10))
+    plt.yticks(range(0, 101, 10))
     plt.show()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cache = set()
 
     for _ in range(20):
