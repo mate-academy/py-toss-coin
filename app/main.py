d9ef651b919dffc01fd27616280c92abbd5744a0
Olexander Lypovenko <fantomasdeft@gmail.com>
2025-01-08 05:56:57 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,31 @@
-# write your code here
+import random
+import matplotlib.pyplot as plt
+
+
+def flip_coin() -> dict:
+    results = {i: 0 for i in range(11)}
+    num_trials = 10000
+
+    for _ in range(num_trials):
+        heads_count = sum(random.choice([0, 1]) for _ in range(10))
+        results[heads_count] += 1
+
+    for key in results:
+        results[key] = round((results[key] / num_trials) * 100, 2)
+
+    return results
+
+
+def draw_gaussian_distribution_graph(data: dict) -> None:
+    number_of_heads = list(data.keys())
+    percentage_of_occurrences = list(data.values())
+
+    plt.plot(number_of_heads,
+             percentage_of_occurrences,
+             color="blue",
+             label="Distribution of the number of eagles")
+    plt.title("Normal distribution")
+    plt.xlabel("Number of eagles")
+    plt.ylabel("Percentage of cases")
+    plt.grid(True)
+    plt.show()
