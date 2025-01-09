6d0bdc280a10cc37044a2ff0c830bbfb250c03f7
unknown <maximuschampion2002@gmail.com>
2025-01-10 01:14:10 +0200

    flip-coin

--- app/main.py
+++ app/main.py
@@ -20,22 +20,23 @@ def flip_experiment(trials: int = 10000) -> dict[int, float]:
 
 
 def draw_gaussian_distribution_graph(data: dict[int, float]) -> None:
-        heads_counts = list(data.keys())
-        drop_percentages = list(data.values())
-        plt.figure(figsize=(8, 6))
-        plt.plot(
+    heads_counts = list(data.keys())
+    drop_percentages = list(data.values())
+    plt.figure(figsize=(8, 6))
+    plt.plot(
             heads_counts, drop_percentages,
-            color="blue", marker="o", markersize=0, linestyle="-"
-        )
-        plt.title("Gaussian Distribution", fontsize=16)
-        plt.xlabel("Heads Count", fontsize=12)
-        plt.ylabel("Drop Percentage (%)", fontsize=12)
-        plt.xlim(0, 10)
-        plt.ylim(0, 100)
-        plt.yticks(range(0, 101, 10))
-        plt.xticks(range(0, 11))
-        plt.grid(axis="y", linestyle="--", alpha=0.5)
-        plt.show()
+        color="blue", marker="o", markersize=0, linestyle="-"
+    )
+    plt.title("Gaussian Distribution", fontsize=16)
+    plt.xlabel("Heads Count", fontsize=12)
+    plt.ylabel("Drop Percentage (%)", fontsize=12)
+    plt.xlim(0, 10)
+    plt.ylim(0, 100)
+    plt.yticks(range(0, 101, 10))
+    plt.xticks(range(0, 11))
+    plt.grid(axis="y", linestyle="--", alpha=0.5)
+    plt.show()
+
 
 data = flip_experiment()
 draw_gaussian_distribution_graph(data)
