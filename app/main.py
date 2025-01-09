38f845a744f13c104957cdcd014ec0e28600c574
unknown <maximuschampion2002@gmail.com>
2025-01-10 01:15:34 +0200

    flip-coin

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ def draw_gaussian_distribution_graph(data: dict[int, float]) -> None:
     drop_percentages = list(data.values())
     plt.figure(figsize=(8, 6))
     plt.plot(
-            heads_counts, drop_percentages,
+        heads_counts, drop_percentages,
         color="blue", marker="o", markersize=0, linestyle="-"
     )
     plt.title("Gaussian Distribution", fontsize=16)
