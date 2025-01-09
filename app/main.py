f023c3b395d0fbb6922a163834cb98c3e2706897
unknown <maximuschampion2002@gmail.com>
2025-01-10 01:19:26 +0200

    flip-coin

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
 import random
-import matplotlib.pyplot as plt
-
 
 def flip_once() -> int:
     heads_count = 0
@@ -8,7 +6,6 @@ def flip_once() -> int:
         if random.choice([0, 1]) == 1:
             heads_count += 1
     return heads_count
-
 
 def flip_experiment(trials: int = 10000) -> dict[int, float]:
     results = {i: 0 for i in range(11)}
@@ -18,25 +15,4 @@ def flip_experiment(trials: int = 10000) -> dict[int, float]:
     percentages = {k: (v / trials) * 100 for k, v in results.items()}
     return percentages
 
-
-def draw_gaussian_distribution_graph(data: dict[int, float]) -> None:
-    heads_counts = list(data.keys())
-    drop_percentages = list(data.values())
-    plt.figure(figsize=(8, 6))
-    plt.plot(
-        heads_counts, drop_percentages,
-        color="blue", marker="o", markersize=0, linestyle="-"
-    )
-    plt.title("Gaussian Distribution", fontsize=16)
-    plt.xlabel("Heads Count", fontsize=12)
-    plt.ylabel("Drop Percentage (%)", fontsize=12)
-    plt.xlim(0, 10)
-    plt.ylim(0, 100)
-    plt.yticks(range(0, 101, 10))
-    plt.xticks(range(0, 11))
-    plt.grid(axis="y", linestyle="--", alpha=0.5)
-    plt.show()
-
-
-data = flip_experiment()
-draw_gaussian_distribution_graph(data)
+print(flip_experiment())
