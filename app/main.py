25ccf9748b0352c2dc4c1ef26d6c1a313d9d1c10
Leo <leonsiypopov@gmail.com>
2025-01-19 23:02:13 +0200

    done

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from random import randint
+import random
 
 
 def flip_coin() -> dict:
@@ -6,11 +6,12 @@ def flip_coin() -> dict:
               4: 0.0, 5: 0.0, 6: 0.0, 7: 0.0,
               8: 0.0, 9: 0.0, 10: 0.0}
 
-    for _ in range(1000):
+    for _ in range(10000):
         head = 0
         for __ in range(10):
-            head += randint(0, 1)
-        counts[head] += 1 / 10
+            if random.choice([0, 1]):
+                head += 1
+        counts[head] += 1 / 100
 
     counts = {key: round(value, 2) for key, value in counts.items()}
 
