46a3532b696b30aeccc03560a1bc4f6c0d29ece1
Greshch <greshchook@gmail.com>
2025-01-20 09:39:14 +0200

    failed

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import random
+import math
+
+
+def get_head_percent(size: int):
+    flips = [bool(random.randint(0, 1)) for _ in range(size)]
+    heads = sum(flips)
+    return round(heads / size * 100, 2)
+
+
+def flip_coin():
+    statistic_dict = {}
+    iterations = 10000
+    size = 10
+    for i in range(iterations):
+        statistic_dict[i] = get_head_percent(size)
+    return statistic_dict
+
