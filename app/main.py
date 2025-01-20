3ee32eb7ba24d40fde45172daa1526d74962baad
Greshch <greshchook@gmail.com>
2025-01-20 19:37:21 +0200

    ok

--- app/main.py
+++ app/main.py
@@ -1,16 +1,21 @@
 import random
 
 
-def get_head_percent(size: int) -> float:
-    flips = [bool(random.randint(0, 1)) for _ in range(size)]
-    heads = sum(flips)
-    return round(heads / size * 100, 2)
+def get_heads_by_flips(size: int) -> int:
+    heads = 0
+    for _ in range(size):
+        heads += random.choice([0, 1])
+    return heads
 
 
 def flip_coin() -> dict:
-    statistic_dict = {}
-    iterations = 10000
+    times = 10000
     size = 10
-    for i in range(iterations):
-        statistic_dict[i] = get_head_percent(size)
-    return statistic_dict
+    result_dict = {}
+    for _ in range(times):
+        key = get_heads_by_flips(size)
+        if result_dict.get(key) is None:
+            result_dict[key] = 0
+        else:
+            result_dict[key] += 1
+    return {key: val / times * 100 for key, val in result_dict.items()}
