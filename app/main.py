cee4cdd9a4d59fed99e782ce7661f6dff97cdbef
Atom-gnomov <gorbach0864213579@gmail.com>
2025-01-23 11:20:23 +0200

    complete + extra

--- app/main.py
+++ app/main.py
@@ -3,29 +3,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def draw_gaussian_distribution_graph() -> None:
+def flip_coin() -> dict:
 
     dicter = {}
-
-    for i in range(10000):
+    for _ in range(10000):
         heads = 0
-        for i in range(10):
-
+        for _ in range(10):
             if random.randint(1, 2) % 2 == 0:
                 heads += 1
         dicter[heads] = dicter.setdefault(heads, 0) + 1
 
-    for i in dicter:
-        dicter[i] = dicter[i] / 10000
-    print(dicter)
+    for key in dicter:
+        dicter[key] = dicter[key] / 10000 * 100
 
-    x_points = np.array(sorted(dicter.keys()))  # Sort the keys for proper plotting
+    return dicter
+
+
+def draw_gaussian_distribution_graph() -> None:
+
+    dicter = flip_coin()
+
+    x_points = np.array(sorted(dicter.keys()))
     y_points = np.array([dicter[key] for key in x_points])
 
-    plt.plot(x_points, y_points)
+    plt.plot(x_points, y_points, marker="o")
     plt.xlabel("Heads count")
-    plt.ylabel("Drop percentage %")
-    plt.title("Gaussian distribution")
+    plt.ylabel("Drop percentage (%)")
+    plt.title("Gaussian Distribution of Heads in 10 Coin Flips")
+    plt.grid(True)
     plt.show()
+
 
 draw_gaussian_distribution_graph()
