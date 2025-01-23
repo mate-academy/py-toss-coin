7f4aa574debfbf7ae4ad64cf5a9bcd96547e3465
Atom-gnomov <gorbach0864213579@gmail.com>
2025-01-23 11:11:33 +0200

    complete + extra

--- app/main.py
+++ app/main.py
@@ -1,1 +1,31 @@
-# write your code here
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def draw_gaussian_distribution_graph() -> None:
+
+    dicter = {}
+
+    for i in range(10000):
+        heads = 0
+        for i in range(10):
+
+            if random.randint(1, 2) % 2 == 0:
+                heads += 1
+        dicter[heads] = dicter.setdefault(heads, 0) + 1
+
+    for i in dicter:
+        dicter[i] = dicter[i] / 10000
+    print(dicter)
+
+    x_points = np.array(sorted(dicter.keys()))  # Sort the keys for proper plotting
+    y_points = np.array([dicter[key] for key in x_points])
+
+    plt.plot(x_points, y_points)
+    plt.xlabel("Heads count")
+    plt.ylabel("Drop percentage %")
+    plt.title("Gaussian distribution")
+    plt.show()
+
+draw_gaussian_distribution_graph()
