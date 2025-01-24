d5dcc5d477973ef23de19fdf20cf6e0d0e818445
misha-cw <bigun.mykhaylo@gmail.com>
2025-01-24 18:10:49 +0200

    deleted draw_gaussian_distribution_graph function

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import random
-import matplotlib.pyplot as plt
 
 
 def flip_coin() -> dict:
@@ -26,18 +25,3 @@ def flip_coin() -> dict:
         statistics[count] += 1
 
     return {num: drop / 100 for num, drop in statistics.items()}
-
-
-def draw_gaussian_distribution_graph() -> None:
-    percents = flip_coin()
-    x, y = list(percents.keys()), list(percents.values())
-
-    plt.plot(x, y)
-    plt.ylim(0, 100)
-    plt.xlim(0, 10)
-
-    plt.xlabel("Head count")
-    plt.ylabel("Drop percentage %")
-    plt.title("Gaussian distribution")
-
-    plt.show()
